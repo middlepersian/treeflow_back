b6a0a9e8f584facd417d1d8ca1fd5e994428673a
Francisco Mondaca <f.mondaca@uni-koeln.de>
2021-08-27 17:29:34 +0200

    morph anno up

--- mpcd/codex/models/token.py
+++ mpcd/codex/models/token.py
@@ -26,82 +26,136 @@ class Pos(models.TextChoices):
     X = "X", "Other"
 
 
-
 class TokenSemantics(models.Model):
     uuid = models.UUIDField(default=uuid_lib.uuid4, editable=False, unique=True)
     meaning = models.CharField(max_length=255)
     description = models.TextField(blank=True)
 
 
+class FeatureValueManager(models.Manager):
+    def get_by_natural_key(self, name):
+        return self.get(name=name)
+
+
 class FeatureValue(models.Model):
     uuid = models.UUIDField(default=uuid_lib.uuid4, editable=False, unique=True)
     # e.g. 'Prs'
-    slug = models.SlugField(unique=True)
+    name = models.SlugField(unique=True)
     # e.g. 'personal or possessive personal pronoun or determiner'
-    description = models.CharField(max_length=150)
+    value = models.CharField(max_length=150, unique=True)
+
+    objects = FeatureValueManager()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'value'], name='featurevalue_name_value'
+            )
+        ]
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+class FeatureManager(models.Manager):
+    def get_by_natural_key(self, name):
+        return self.get(name=name)
+
 
 class Feature(models.Model):
     uuid = models.UUIDField(default=uuid_lib.uuid4, editable=False, unique=True)
     # e.g. "PronType"
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
     # e.g. "pronominal type"
-    description = models.CharField(max_length=100)
-    
-    values = models.ForeignKey(FeatureValue, blank=True, null=True)
+    value = models.CharField(max_length=100, unique=True)
+
+    objects = FeatureManager()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'value'], name='feature_name_value'
+            )
+        ]
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
 
 class MorphologicalAnnotation(models.Model):
     uuid = models.UUIDField(default=uuid_lib.uuid4, editable=False, unique=True)
     pos = models.CharField(max_length=6, choices=Pos.choices, null=True)
-    feature = models.ForeignKey(Feature, on_delete=models.CASCADE, null=True)    
- 
+    feature = models.ForeignKey(Feature, on_delete=models.CASCADE, null=True)
+    feature_value = models.ForeignKey(FeatureValue, on_delete=models.CASCADE, null=True)
+
     class Meta:
         constraints = [
             models.CheckConstraint(
-                name="%(app_label)s_%(class)s_pos_valid",
+                name="valid_pos",
                 check=models.Q(pos__in=Pos.values),
-            )]
-
+            ),
+            models.UniqueConstraint(
+                fields=['pos', 'feature', 'feature_value'], name='pos_feature_feature_value'
+            )
+        ]
 
     def __str__(self):
-        return self.pos
+        return '{} {} {}'.format(self.pos, self.feature, self.feature_value)
 
 
-        
 class DependencyRelation(models.TextChoices):
-        acl = 'acl', 'clausal modifier of noun (adnominal clause)'
-        advcl = 'advcl', 'adverbial clause modifier'
-        advmod = "advmod", "adverbial modifier"
-        amod = "amod", "adjectival modifier"
-        appos = "appos", "appositional modifier"
-        aux = "aux", "auxiliary"
-        case = "case", "case marking"
-        cc = "cc", "coordinating conjunction"
-        ccomp = "ccomp", "clausal complement"
-        compound = "compound", "compound"
-        conj = "conj", "conjunct"
-        cop = "cop", "copula"
-        det = "det", "determiner"
-        discourse = "discourse", "discourse element"
-        fixed = "fixed", "fixed multiword expression"
-        iobj = "iobj", "indirect object"
-        mark = "mark", "marker"
-        nmod = "nmod", "nominal modifier"
-        nsubj = "nsubj", "nominal subject"
-        nummod = "nummod", "numeric modifier"
-        obj = "obj", "object"
-        obl = "obl", "oblique nominal"
-        root = "root", "root"
+    acl = 'acl', 'clausal modifier of noun (adnominal clause)'
+    advcl = 'advcl', 'adverbial clause modifier'
+    advmod = "advmod", "adverbial modifier"
+    amod = "amod", "adjectival modifier"
+    appos = "appos", "appositional modifier"
+    aux = "aux", "auxiliary"
+    case = "case", "case marking"
+    cc = "cc", "coordinating conjunction"
+    ccomp = "ccomp", "clausal complement"
+    compound = "compound", "compound"
+    conj = "conj", "conjunct"
+    cop = "cop", "copula"
+    det = "det", "determiner"
+    discourse = "discourse", "discourse element"
+    fixed = "fixed", "fixed multiword expression"
+    iobj = "iobj", "indirect object"
+    mark = "mark", "marker"
+    nmod = "nmod", "nominal modifier"
+    nsubj = "nsubj", "nominal subject"
+    nummod = "nummod", "numeric modifier"
+    obj = "obj", "object"
+    obl = "obl", "oblique nominal"
+    root = "root", "root"
+
+
+class DependencyManager(models.Manager):
+    def get_by_natural_key(self, head, rel):
+        return self.get(head=head, rel=rel)
+
 
 class Dependency(models.Model):
     uuid = models.UUIDField(default=uuid_lib.uuid4, editable=False)
-    head = models.SmallIntegerField()
-    
 
-    ## TODO: add DB constraint 
-    dependency_relation = models.CharField(max_length=9, choices=DependencyRelation.choices)
+    head = models.SmallIntegerField()
+    # TODO: add DB constraint
+    rel = models.CharField(max_length=9, choices=DependencyRelation.choices)
+
+    objects = DependencyManager()
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name="valid_rel",
+                check=models.Q(rel__in=DependencyRelation.values),
+            ),
+            models.UniqueConstraint(
+                fields=['head', 'rel'], name='head_rel'
+            )
+        ]
 
     def __str__(self):
-        return str(self.head) + '    ' + self.dependency_relation
+        return '{} {}'.format(str(self.head), self.rel)
 
 
 class SyntacticAnnotation(models.Model):
@@ -109,7 +163,7 @@ class SyntacticAnnotation(models.Model):
     dependency = models.ForeignKey(Dependency, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
-        return str(self.dependency.head) + '    ' + self.dependency.dependency_relation
+        return '{}'.format(self.dependency)
 
 
 class Token(models.Model):
@@ -119,8 +173,8 @@ class Token(models.Model):
     transliteration = models.TextField(blank=True)
     lemma = models.ForeignKey(Entry, on_delete=models.DO_NOTHING, null=True, blank=True)
 
-    morph_annotations = models.ForeignKey(MorphologicalAnnotation, on_delete=models.CASCADE, null=True)
-    syntax_annotations = models.ForeignKey(SyntacticAnnotation, on_delete=models.CASCADE, null=True)
+    morph_annotations = models.ForeignKey(MorphologicalAnnotation, on_delete=models.CASCADE, null=True, blank=True)
+    syntax_annotations = models.ForeignKey(SyntacticAnnotation, on_delete=models.CASCADE, null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
