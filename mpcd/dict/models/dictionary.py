805a169d4fd00808953879944ee6f95bf36280c9
Francisco Mondaca <f.mondaca@uni-koeln.de>
2021-09-07 09:48:20 +0200

    model and migrations up

--- mpcd/dict/models/dictionary.py
+++ mpcd/dict/models/dictionary.py
@@ -1,6 +1,7 @@
 import uuid as uuid_lib
 
 from django.db import models
+from django.db.models.fields import CharField
 from django.urls import reverse
 from simple_history.models import HistoricalRecords
 
@@ -15,6 +16,24 @@ class Dictionary(models.Model):
     def __str__(self):
         return self.name
 
+
+class LangCh(models.TextChoices):
+    #iso 639-3
+    akk = 'akk', 'Akkadian'
+    arc = 'arc', 'Imperial Aramaic (700-300 BCE), Official Aramaic (700-300 BCE)'
+    ave = 'ave', 'Avestan'
+    grc = 'grc', 'Ancient Greek (to 1453)'
+    xpr = 'xpr', 'Parthian'
+
+class Lang(models.Model):
+    language =  models.CharField(max_length=3, choices=LangCh.choices, unique=True)   
+    def __str__(self):
+        return '{}'.format(self.language)
+
+class LoanWord(models. Model):
+    id = models.UUIDField(primary_key=True, default=uuid_lib.uuid4, editable=False)
+    word = models.CharField(unique=True, max_length=30, null=True)
+    language = models.ForeignKey(Lang, on_delete=models.CASCADE, null=True)
 
 class MeaningEnglish(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid_lib.uuid4, editable=False)
