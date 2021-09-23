47df9b678f51d883859b3717e06b9f500e2a9e33
Moritz Eßer <info@horatiuslampenschirm.de>
2021-09-23 12:41:56 +0200

    API-Endpoints + Simple Search-Test

--- mpcd/dict/serializers.py
+++ mpcd/dict/serializers.py
@@ -1,15 +1,50 @@
 from rest_framework import serializers
-from .models import Entry, Dictionary
+from .models import Entry, Dictionary, Lang, Translation, Category, Word, LoanWord, Reference, Definition
 
 class EntrySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Entry
-        fields = ('pk','lemma', 'dict')
+        fields = ('pk','lemma', 'dict','loanword','translation','definition','category','literature','comment','cross_reference')
 
 
 class DictionarySerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Dictionary
         fields = ('pk','name', 'slug')
+
+
+class LangSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lang
+        fields = '__all__'
+
+class TranslationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Translation
+        fields = ('pk','language','meaning')
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class WordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Word
+        fields = ('pk','word', 'language')
+
+class LoanWordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LoanWord
+        fields = ('pk','word', 'language','translation')
+
+class ReferenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reference
+        fields = ('pk','reference','url')
+
+class DefinitionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Definition
+        fields = ('pk','definition','language')
--- mpcd/dict/views.py
+++ mpcd/dict/views.py
@@ -1,6 +1,8 @@
-from rest_framework import viewsets
-from .models import Entry, Dictionary
-from .serializers import EntrySerializer, DictionarySerializer
+from mpcd.dict.models.dictionary import Category, Definition, Reference, Translation
+from rest_framework import viewsets, filters
+from .models import Entry, Dictionary, Word, LoanWord, Lang
+from .serializers import DefinitionSerializer, EntrySerializer, DictionarySerializer, ReferenceSerializer, WordSerializer, \
+    LoanWordSerializer, LangSerializer, TranslationSerializer, CategorySerializer
 from .permissions import IsAuthorOrReadOnly
 
 
@@ -12,5 +14,41 @@ class EntryViewSet(viewsets.ModelViewSet):
 class DictionaryViewSet(viewsets.ModelViewSet):
     queryset = Dictionary.objects.all()
     serializer_class = DictionarySerializer
-    permission_classes = (IsAuthorOrReadOnly,)    
+    permission_classes = (IsAuthorOrReadOnly,)
 
+class WordViewSet(viewsets.ModelViewSet):
+    queryset = Word.objects.all()
+    serializer_class = WordSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['word','language__language']
+    permission_classes = (IsAuthorOrReadOnly,)
+
+class LoanWordViewSet(viewsets.ModelViewSet):
+    queryset = LoanWord.objects.all()
+    serializer_class = LoanWordSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+
+class LangViewSet(viewsets.ModelViewSet):
+    queryset = Lang.objects.all()
+    serializer_class = LangSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+
+class TranslationViewSet(viewsets.ModelViewSet):
+    queryset = Translation.objects.all()
+    serializer_class = TranslationSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+
+class DefinitionViewSet(viewsets.ModelViewSet):
+    queryset = Definition.objects.all()
+    serializer_class = DefinitionSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+
+class ReferenceViewSet(viewsets.ModelViewSet):
+    queryset = Reference.objects.all()
+    serializer_class = ReferenceSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
