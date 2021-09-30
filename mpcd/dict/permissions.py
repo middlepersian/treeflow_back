f281b7e5a8093b4967cabf8566e7fe284414348b
Moritz Eßer <info@horatiuslampenschirm.de>
2021-09-30 13:25:53 +0200

    Update Permissions API

--- mpcd/dict/permissions.py
+++ mpcd/dict/permissions.py
@@ -5,3 +5,13 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+        if request.user.is_superuser:
+            return True
+        return False
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.user.is_superuser:
+            return True
+        return False
