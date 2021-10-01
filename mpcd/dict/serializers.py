5c0d59a8456d9d77e9c455a3305779f6d4838e58
Francisco Mondaca <f.mondaca@uni-koeln.de>
2021-10-01 13:41:47 +0200

    serializers drf-writable-nested

--- mpcd/dict/serializers.py
+++ mpcd/dict/serializers.py
@@ -1,50 +1,80 @@
 from rest_framework import serializers
+from drf_writable_nested.serializers import WritableNestedModelSerializer
 from .models import Entry, Dictionary, Lang, Translation, Category, Word, LoanWord, Reference, Definition
-
-class EntrySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Entry
-        fields = ('pk','lemma', 'dict','loanword','translation','definition','category','literature','comment','cross_reference')
 
 
 class DictionarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Dictionary
-        fields = ('pk','name', 'slug')
+        fields = ('pk', 'name', 'slug')
 
 
 class LangSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lang
-        fields = '__all__'
+        fields = ('pk', 'language', )
 
-class TranslationSerializer(serializers.ModelSerializer):
+
+class TranslationSerializer(WritableNestedModelSerializer):
+    language = LangSerializer(allow_null=True)
+
     class Meta:
         model = Translation
-        fields = ('pk','language','meaning')
+        fields = ('pk', 'language', 'meaning')
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
 
-class WordSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Word
-        fields = ('pk','word', 'language')
 
-class LoanWordSerializer(serializers.ModelSerializer):
+class LoanWordSerializer(WritableNestedModelSerializer):
+    language = LangSerializer(allow_null=True)
+    translation = TranslationSerializer(many=True)
+
     class Meta:
         model = LoanWord
-        fields = ('pk','word', 'language','translation')
+        fields = ('pk', 'word', 'language', 'translation')
+
 
 class ReferenceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reference
-        fields = ('pk','reference','url')
+        fields = ('pk', 'reference', 'url')
 
-class DefinitionSerializer(serializers.ModelSerializer):
+
+class DefinitionSerializer(WritableNestedModelSerializer):
+    language = LangSerializer(allow_null=True)
+
     class Meta:
         model = Definition
-        fields = ('pk','definition','language')
+        fields = ('pk', 'definition', 'language')
+
+
+class EntrySerializer(WritableNestedModelSerializer):
+
+    dict = DictionarySerializer()
+    loanword = LoanWordSerializer(allow_null=True)
+    translation = TranslationSerializer(many=True)
+    definition = DefinitionSerializer(many=True)
+    category = CategorySerializer(many=True)
+    literature = ReferenceSerializer(many=True)
+    #cross_reference = WordSerializer(many=True)
+
+    class Meta:
+        model = Entry
+        fields = ('pk', 'dict', 'loanword', 'translation', 'definition',
+                  'category', 'literature', 'comment', 'cross_reference')
+
+
+class WordSerializer(WritableNestedModelSerializer):
+
+    language = LangSerializer(allow_null=True)
+
+    # Reverse OneToOne relationship
+    entry = EntrySerializer()
+
+    class Meta:
+        model = Word
+        fields = ('pk', 'word', 'language', 'entry')
