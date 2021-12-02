891c8ebee94381ee7376fe49415f73e4b0f8702e
Francisco Mondaca <f.mondaca@uni-koeln.de>
2021-12-02 09:49:32 +0100

    views up

--- mpcd/corpus/views.py
+++ mpcd/corpus/views.py
@@ -1,9 +1,16 @@
 from rest_framework import viewsets
 
-from .models import BibEntry
+from .models import Author, BibEntry, Codex, Folio, Line
 
-from .serializers import BibEntrySerializer
+from .serializers import AuthorSerializer, BibEntrySerializer, CodexSerializer, FolioSerializer, LineSerializer
 from .permissions import IsAuthorOrReadOnly
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    search_fields = ['name', 'last_name']
+    permission_classes = (IsAuthorOrReadOnly,)
 
 
 class BibEntryViewSet(viewsets.ModelViewSet):
@@ -11,3 +18,25 @@ class BibEntryViewSet(viewsets.ModelViewSet):
     serializer_class = BibEntrySerializer
     search_fields = ['title', 'year']
     permission_classes = (IsAuthorOrReadOnly,)
+
+
+class CodexViewSet(viewsets.ModelViewSet):
+    queryset = Codex.objects.all()
+    serializer_class = CodexSerializer
+    search_fields = ['name', 'slug', 'description', 'scribe', 'library', 'signature', 'facisimile']
+    permission_classes = (IsAuthorOrReadOnly,)
+
+
+class FolioViewSet(viewsets.ModelViewSet):
+    queryset = Folio.objects.all()
+    serializer_class = FolioSerializer
+    search_fields = ['name', 'codex', 'comment']
+    permission_classes = (IsAuthorOrReadOnly,)
+
+
+class LineViewSet(viewsets.ModelViewSet):
+    queryset = Line.objects.all()
+    serializer_class = LineSerializer
+    search_fields = ['number', 'folio', 'comment']
+    permission_classes = (IsAuthorOrReadOnly,)
+
