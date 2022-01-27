4b52adc1cc04118985bfe4fd1915e1b640b8342c
fmondaca <fmondaca@baker.dh.uni-koeln.de>
2022-01-27 10:10:40 +0100

    word and lang contraint added to lemma

--- mpcd/dict/models/lemma.py
+++ mpcd/dict/models/lemma.py
@@ -6,9 +6,16 @@ from .language import Language
 
 class Lemma(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid_lib.uuid4, editable=False)
-    word = models.CharField(unique=True, max_length=100)
+    word = models.CharField(max_length=100)
     language = models.ForeignKey(Language, on_delete=models.CASCADE, blank=True,
                                  null=True, related_name='lemma_language')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['word', 'language'], name='word_language'
+            )
+        ]
 
     history = HistoricalRecords()
 
