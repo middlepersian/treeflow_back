3b3533fdc53b0dc7421dcea213709fa8061f7de6
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-02-15 14:13:10 +0100

    line and position constraint added

--- mpcd/corpus/models/codex_token.py
+++ mpcd/corpus/models/codex_token.py
@@ -4,11 +4,17 @@ from .line import Line
 from simple_history.models import HistoricalRecords
 
 
-# TODO: write constrain for position
 class CodexToken(Token):
     line = models.ForeignKey(Line, on_delete=models.SET_NULL, null=True, related_name='codex_token_line')
     position = models.PositiveSmallIntegerField(null=True)
     history = HistoricalRecords(inherit=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['line', 'position'], name='line_position'
+            )
+        ]
 
     def __str__(self):
         return self.transcription
