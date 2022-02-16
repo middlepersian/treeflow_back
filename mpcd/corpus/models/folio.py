012f12483c5f61bc3baa6df1983173083ea7763c
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-02-16 11:56:45 +0100

    pointing now to facsimile instead of codex

--- mpcd/corpus/models/folio.py
+++ mpcd/corpus/models/folio.py
@@ -1,22 +1,22 @@
 import uuid as uuid_lib
 from django.db import models
 from simple_history.models import HistoricalRecords
-from .codex import Codex
+from .facsimile import Facsimile
 
 
 class Folio(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid_lib.uuid4, editable=False)
-    codex = models.ForeignKey(Codex, on_delete=models.CASCADE, related_name='folio_codex')
-    position_in_codex = models.PositiveSmallIntegerField(default=0)
     identifier = models.CharField(max_length=100)
+    facsimile = models.ForeignKey(Facsimile, on_delete=models.CASCADE, null=True,
+                                  blank=True, related_name='folio_facsimile')
     comment = models.CharField(max_length=255, blank=True)
-    history = HistoricalRecords()
     previous = models.OneToOneField('self',
                                     related_name='next',
                                     blank=True,
                                     null=True,
                                     on_delete=models.DO_NOTHING)
-  
+
+    history = HistoricalRecords()
 
     def __str__(self):
         return '{}'.format(self.name)
