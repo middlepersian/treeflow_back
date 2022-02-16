93b60ac408e240ce85ed727729da1dc11ec03a5e
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-02-16 11:52:54 +0100

    constraint for title and year created

--- mpcd/corpus/models/bibliography.py
+++ mpcd/corpus/models/bibliography.py
@@ -5,10 +5,18 @@ from .author import Author
 
 class BibEntry(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid_lib.uuid4, editable=False)
-    authors = models.ManyToManyField(Author, related_name="bib_entry_authors")
+    authors = models.ManyToManyField(Author, related_name="bib_entry_authors", blank=True)
     title = models.CharField(max_length=100)
     year = models.PositiveSmallIntegerField()
+
     url = models.URLField(blank=True, null=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'year'], name='title_year_unique'
+            )
+        ]
 
     def get_authors(self):
         return "|\n".join([p.last_name + ' , ' + p.name for p in self.authors.all()])
