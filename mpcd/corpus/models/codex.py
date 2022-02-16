43307f0c14a3f9d016f42b5fb79140d034502195
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-02-16 11:59:16 +0100

    reference to facsimiles removed

--- mpcd/corpus/models/codex.py
+++ mpcd/corpus/models/codex.py
@@ -2,7 +2,6 @@ from django.db import models
 from simple_history.models import HistoricalRecords
 from .source import Source
 from .author import Author
-from .bibliography import BibEntry
 
 
 class CodexCh(models.TextChoices):
@@ -38,8 +37,6 @@ class Codex(Source):
     library = models.CharField(max_length=100,  blank=True)
     signature = models.CharField(max_length=100,  blank=True)
     scribes = models.ManyToManyField(Author, blank=True, related_name='codex_scribes')
-    facsimiles = models.ManyToManyField(BibEntry,
-                                        blank=True, related_name='codex_facsimiles')
 
     history = HistoricalRecords()
 
