480fc46b10498afd0eb8f6b04fcbbca98d876702
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-03-25 14:46:34 +0100

    author and bibliography schemas unicode normalized

--- mpcd/corpus/schemas/author.py
+++ mpcd/corpus/schemas/author.py
@@ -5,7 +5,7 @@ from graphql_relay import from_global_id
 import graphene_django_optimizer as gql_optimizer
 
 from mpcd.corpus.models import Author
-
+from mpcd.utils.normalize import to_nfc
 
 
 # import the logging library
@@ -24,8 +24,8 @@ class AuthorNode(DjangoObjectType):
 
 
 class AuthorInput(InputObjectType):
-    name = String()
-    last_name = String()
+    name = String(required=True)
+    last_name = String(required=True)
 
 # Queries
 
@@ -50,33 +50,34 @@ class CreateAuthor(relay.ClientIDMutation):
     errors = List(String)
 
     @classmethod
-    def mutate_and_get_payload(cls, root, info, name, last_name):
-        logger.error('ROOT: {}'.format(root))
-        # check that author does not exist same name and last name
-        if Author.objects.filter(name=name, last_name=last_name).exists():
-            return cls(success=False, errors=['Author already exists'])
+    def mutate_and_get_payload(cls, root, info, **input):
+        logger.error('author_inpuut: {}'.format(input))
 
-        else:
-            author_instance = Author.objects.create(name=name, last_name=last_name)
-            author_instance.save()
-            return cls(author=author_instance, success=True)
+        # check if author exists, if not create it
+        author_instance, author_created = Author.objects.get_or_create(
+            name=to_nfc(input.get('name')), last_name=to_nfc(input.get('last_name')))
+
+        return cls(author=author_instance, success=True, errors=None)
 
 
 class UpdateAuthor(relay.ClientIDMutation):
     class Input:
-        id = ID()
+        id = ID(required=True)
         name = String(required=True)
         last_name = String(required=True)
 
     author = Field(AuthorNode)
 
     @ classmethod
-    def mutate_and_get_payload(cls, root, info, name, last_name, id):
-        author = Author.objects.get(pk=from_global_id(id)[1])
-        author.name = name
-        author.last_name = last_name
-        author.save()
-        return cls(author=author)
+    def mutate_and_get_payload(cls, root, info, **input):
+        if Author.objects.filter(pk=from_global_id(id)[1]).exists():
+            author_instance = Author.objects.get(pk=from_global_id(id)[1])
+            author_instance.name = to_nfc(input.get('name'))
+            author_instance.last_name = to_nfc(input.get('last_name'))
+            author_instance.save()
+            return cls(author=author_instance, success=True)
+        else:
+            return cls(errors=['Author ID does not exist'], success=False)
 
 
 class DeleteAuthor(relay.ClientIDMutation):
--- mpcd/corpus/schemas/bibliography.py
+++ mpcd/corpus/schemas/bibliography.py
@@ -6,6 +6,7 @@ from mpcd.corpus.models import Author, BibEntry
 from mpcd.corpus.schemas.author import AuthorInput
 
 import graphene_django_optimizer as gql_optimizer
+from mpcd.utils.normalize import to_nfc
 
 
 # import the logging library
@@ -40,10 +41,12 @@ class Query(ObjectType):
         return gql_optimizer.query(BibEntry.objects.all(), info)
 
 # Mutations
+
+
 class CreateBibEntry(relay.ClientIDMutation):
     class Input:
-        title = String()
-        year = Int()
+        title = String(required=True)
+        year = Int(required=True)
         authors = List(AuthorInput)
 
     bibentry = Field(BibEntryNode)
@@ -52,36 +55,25 @@ class CreateBibEntry(relay.ClientIDMutation):
 
     @classmethod
     def mutate_and_get_payload(cls, root, info, **input):
-        # check that bybentry does not exist same title and year
         logger.error("input: {}".format(input))
+        bibentry_instance, bibentry_created = BibEntry.objects.get_or_create(
+            title=to_nfc(input.get('title')), year=to_nfc(input.get('year')))
 
-        if input.get('title', None) is not None:
-            title = input.get('title')
-            bibentry_instance = BibEntry.objects.create(title=title)
-        else:
-            return cls(success=False, errors=['title is required'])
-
-        if input.get('year', None) is not None:
-            year = input.get('year')
-            logger.error("year: {}".format(year))
-            bibentry_instance.year = year
-
-        if input.get('authors', None) is not None:
-            authors = input.get('authors', None)
-            for author in authors:
-                author_instance, author_created = Author.objects.get_or_create(
-                    name=author.get('name', None),
-                    last_name=author.get('last_name', None))
-                bibentry_instance.authors.add(author_instance)
+        authors = input.get('authors', None)
+        for author in authors:
+            author_instance, author_created = Author.objects.get_or_create(
+                name=to_nfc(author.get('name', None)),
+                last_name=to_nfc(author.get('last_name', None)))
+            bibentry_instance.authors.add(author_instance)
         bibentry_instance.save()
         return cls(bibentry=bibentry_instance, success=True)
 
 
 class UpdateBibEntry(relay.ClientIDMutation):
     class Input:
-        id = ID()
-        title = String()
-        year = Int()
+        id = ID(required=True)
+        title = String(required=True)
+        year = Int(required=True)
         authors = List(AuthorInput)
 
     bibentry = Field(BibEntryNode)
@@ -91,30 +83,25 @@ class UpdateBibEntry(relay.ClientIDMutation):
     @classmethod
     def mutate_and_get_payload(cls, root, info, **input):
 
-        if input.get('id', None) is not None:
-            if BibEntry.objects.filter(pk=from_global_id(id)[1]).exists():
-                bibentry_instance = BibEntry.objects.get(pk=from_global_id(id)[1])
-            else:
-                return cls(success=False, errors=['bibentry does not exist'])
-
-            if input.get('title', None) is not None:
-                title = input.get('title', None)
-                bibentry_instance.title = title
-
-            if input.get('authors', None) is not None:
-                bibentry_instance.authors.clear()
-                authors = input.get('authors', None)
-                for author in authors:
-                    author_instance, author_created = Author.objects.get_or_create(
-                        first_name=author.get('first_name', None),
-                        last_name=author.get('last_name', None))
-                    bibentry_instance.authors.add(author_instance)
-
-            bibentry_instance.save()
-            return cls(bibentry=bibentry_instance, success=True)
-
+        if BibEntry.objects.filter(pk=from_global_id(id)[1]).exists():
+            bibentry_instance = BibEntry.objects.get(pk=from_global_id(id)[1])
         else:
-            return cls(success=False, errors=['id is required'])
+            return cls(success=False, errors=['BibEntry does not exist'])
+
+        bibentry_instance.title = to_nfc(input.get('title'))
+        bibentry_instance.year = to_nfc(input.get('year'))
+
+        if input.get('authors', None) is not None:
+            bibentry_instance.authors.clear()
+            authors = input.get('authors', None)
+            for author in authors:
+                author_instance, author_created = Author.objects.get_or_create(
+                    first_name=to_nfc(author.get('first_name', None)),
+                    last_name=to_nfc(author.get('last_name', None)))
+                bibentry_instance.authors.add(author_instance)
+
+        bibentry_instance.save()
+        return cls(bibentry=bibentry_instance, success=True)
 
 
 class DeleteBibEntry(relay.ClientIDMutation):
