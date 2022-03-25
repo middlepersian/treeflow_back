c089070b035cd03203afee4a82b9f2f862081d58
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-03-25 12:51:55 +0100

    constraint added

--- mpcd/dict/models/translation.py
+++ mpcd/dict/models/translation.py
@@ -8,13 +8,17 @@ from .language import LanguageChoices
 class Translation(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid_lib.uuid4, editable=False)
     language = models.CharField(max_length=3, choices=LanguageChoices.choices, null=True, blank=True)
-
     text = models.TextField(unique=True)
 
     history = HistoricalRecords()
 
     class Meta:
         ordering = ['text']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['language', 'text'], name='translation_language_text'
+            )
+        ]
 
     def __str__(self):
         return self.text
