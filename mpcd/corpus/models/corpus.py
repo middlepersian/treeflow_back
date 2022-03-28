6d1307b43b413447d514a78964dd7d1521758916
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-03-28 17:52:13 +0200

    unique contraint added

--- mpcd/corpus/models/corpus.py
+++ mpcd/corpus/models/corpus.py
@@ -12,9 +12,17 @@ class CorpusManager(models.Manager):
 
 class Corpus(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid_lib.uuid4, editable=False)
-    name = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=10, unique=True)
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=10)
     history = HistoricalRecords()
+
+    class Meta:
+        ordering = ['name']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'slug'], name='corpus_name_slug'
+            )
+        ]
 
     def __str__(self):
         return self.slug
