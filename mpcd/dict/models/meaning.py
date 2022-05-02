75f140f9b5a31cead224c59961c920da32677871
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-05-02 16:01:30 +0200

    semantic and meaning as new models

--- mpcd/dict/models/meaning.py
+++ mpcd/dict/models/meaning.py
@@ -1,7 +1,6 @@
 import uuid as uuid_lib
 from django.db import models
 from simple_history.models import HistoricalRecords
-
 from .language import LanguageChoices
 
 
@@ -10,3 +9,13 @@ class Meaning(models.Model):
     meaning = models.TextField(null=True, blank=True)
     language = models.CharField(max_length=10, choices=LanguageChoices.choices)
     history = HistoricalRecords()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['meaning', 'language'], name='meaning_language_meaning'
+            )]
+        ordering = ['meaning']
+
+    def __str__(self):
+        return '{}'.format(self.meaning)
--- mpcd/dict/models/semantic.py
+++ mpcd/dict/models/semantic.py
@@ -4,12 +4,14 @@ from simple_history.models import HistoricalRecords
 
 from .meaning import Meaning
 from .term_tech import TermTech
+from .entry import Entry
 
 
 class Semantic(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid_lib.uuid4, editable=False)
+    entry = models.ForeignKey(Entry, on_delete=models.SET_NULL, null=True, related_name="semantic_entry")
     meanings = models.ManyToManyField(Meaning, blank=True, related_name='semantic_meanings')
-    term_techs = models.ManyToManyField(TermTech, blank=True, related_name='semantic_term_tech')
+    term_techs = models.ManyToManyField(TermTech, blank=True, related_name='semantic_term_techs')
     comment = models.TextField(null=True, blank=True)
     history = HistoricalRecords()
     # TODO add pointers to taxonomy
