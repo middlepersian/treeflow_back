db9d5bedf8bb61a02a2488e84725d315ab1b0051
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-05-05 14:54:45 +0200

    related_meanings added

--- mpcd/dict/models/lemma.py
+++ mpcd/dict/models/lemma.py
@@ -2,6 +2,7 @@ import uuid as uuid_lib
 from django.db import models
 from simple_history.models import HistoricalRecords
 from .language import LanguageChoices
+from .meaning import Meaning
 
 
 class Lemma(models.Model):
@@ -9,6 +10,7 @@ class Lemma(models.Model):
     word = models.CharField(max_length=100)
     language = models.CharField(max_length=3, choices=LanguageChoices.choices, null=True, blank=True)
     related_lemmas = models.ManyToManyField('self', blank=True, related_name='lemma_related_lemmas')
+    related_meanings = models.ManyToManyField(Meaning, blank=True, related_name='lemma_related_meanings')
     comment = models.TextField(null=True, blank=True)
 
 
