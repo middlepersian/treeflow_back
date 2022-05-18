666b1e15ebc9c68a41eee01bb79408a92efc37fd
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-05-18 14:13:01 +0200

    - migrations cleanup - custom_source added - source up

--- mpcd/corpus/models/source.py
+++ mpcd/corpus/models/source.py
@@ -7,7 +7,7 @@ from simple_history.models import HistoricalRecords
 class Source(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid_lib.uuid4, editable=False)
 
-    title = models.CharField(max_length=100, default="")
+    title = models.CharField(max_length=100, blank=True, null=True)
     slug = models.SlugField(max_length=10, unique=True)
     description = models.CharField(max_length=255, blank=True)
 
