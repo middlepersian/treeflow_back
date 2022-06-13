ecf91f1d8692ae3158769ff18e9929cea3face9f
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-06-13 10:50:32 +0200

    source model up

--- mpcd/corpus/models/source.py
+++ mpcd/corpus/models/source.py
@@ -7,12 +7,12 @@ from simple_history.models import HistoricalRecords
 class Source(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid_lib.uuid4, editable=False)
 
-    slug = models.SlugField(max_length=10, unique=True)
-    bib_entry = models.ForeignKey(BibEntry, on_delete=models.DO_NOTHING, null=True,
+    identifier = models.CharField(max_length=30, null=True, blank=True)
+    bib_entry = models.ForeignKey(BibEntry, on_delete=models.SET_NULL, null=True,
                                   blank=True, related_name='source_bib_entry')
     description = models.TextField(null=True, blank=True)
 
     history = HistoricalRecords()
 
     def __str__(self):
-        return '{}'.format(self.slug)
+        return '{}'.format(self.identifier)
