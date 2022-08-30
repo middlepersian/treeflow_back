4902cb391176965e0eec0e74e775c6736aac511f
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-08-30 17:42:44 +0200

    graphene schemas removed

--- mpcd/dict/schemas/__init__.py
+++
@@ -1,7 +0,0 @@
-from .dictionary import DictionaryNode, DictionaryInput
-from .term_tech import TermTechNode, TermTechInput
-from .reference import ReferenceNode, ReferenceInput
-from .lemma import LemmaNode, LemmaInput
-from .meaning import MeaningNode, MeaningInput
-from .semantic import SemanticNode, SemanticInput
-from .language_enum import Language
