145a5d84dd98ce539de418db659ef5532fa48e7c
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-09-28 09:59:45 +0200

    comment null solved

--- mpcd/corpus/types/token_comment.py
+++ mpcd/corpus/types/token_comment.py
@@ -1,12 +1,20 @@
 from strawberry_django_plus import gql
 from strawberry_django_plus.mutations import resolvers
 from strawberry_django_plus.gql import relay
-from typing import List
+from strawberry.lazy_type import LazyType
+from typing import List, TYPE_CHECKING, Optional
 from mpcd.corpus import models
+if TYPE_CHECKING:
+    from mpcd.corpus.types.user import User
 
 
 @gql.django.type(models.TokenComment)
 class TokenComment:
-    uncertain: List[str]
-    to_discuss: List[str]
-    new_suggestion: List[str]
+    id: gql.auto
+    user: LazyType['User', 'mpcd.corpus.types.user']
+    text: gql.auto
+    created_at: gql.auto
+    updated_at: gql.auto
+    uncertain: Optional[List[Optional[str]]]
+    to_discuss: Optional[List[Optional[str]]]
+    new_suggestion:  Optional[List[Optional[str]]]
