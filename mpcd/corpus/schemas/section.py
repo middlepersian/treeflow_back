95a5ed10390d2609ee804b87582438cd8d64a010
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-10-25 11:17:35 +0200

    set_previous_section in progress

--- mpcd/corpus/schemas/section.py
+++ mpcd/corpus/schemas/section.py
@@ -2,6 +2,7 @@ from strawberry_django_plus import gql
 from strawberry_django_plus.gql import relay
 from strawberry_django_plus.optimizer import DjangoOptimizerExtension
 from typing import Optional
+from django.core.exceptions import ValidationError
 
 from mpcd.corpus.types.section import Section, SectionInput, SectionPartial
 
@@ -17,6 +18,14 @@ class Mutation:
     create_section: Section = gql.django.create_mutation(SectionInput)
     update_section: Section = gql.django.update_mutation(SectionPartial)
     delete_section: Section = gql.django.delete_mutation(gql.NodeInput)
+
+    @gql.django.input_mutation
+    def set_previous_section(self, info, current_section_id: relay.GlobalID, previous_section_id: relay.GlobalID) -> Section:
+        obj = id.resolve_node(info)
+        if obj.some_field == "some_value":
+            raise ValidationError("Cannot update obj with some_value")
+
+        return obj
 
 
 schema = gql.Schema(query=Query, mutation=Mutation, extensions=[DjangoOptimizerExtension])
