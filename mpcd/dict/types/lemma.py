98dab11e63cd792b8041052ecd1f423bfd68aa9a
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-10-26 10:24:17 +0200

    lazylist deprecated

--- mpcd/dict/types/lemma.py
+++ mpcd/dict/types/lemma.py
@@ -1,23 +1,27 @@
 
+from strawberry import lazy
 from strawberry_django_plus import gql
-from strawberry_django_plus.mutations import resolvers
 from strawberry_django_plus.gql import relay
-from strawberry.lazy_type import LazyType
-from typing import List, TYPE_CHECKING
+from typing import List, TYPE_CHECKING, Annotated
 from mpcd.dict import models
+
+if TYPE_CHECKING:
+    from mpcd.corpus.types.token import Token
+    from mpcd.corpus.types.comment import Comment
+    from .meaning import Meaning
 
 
 @gql.django.type(models.Lemma)
 class Lemma(relay.Node):
 
-    token_lemmas:  relay.Connection[LazyType['Token', 'mpcd.corpus.types.token']]
+    token_lemmas:  relay.Connection[Annotated['Token', lazy('mpcd.corpus.types.token')]]
 
     #id: gql.auto
     word: gql.auto
     language: gql.auto
     related_lemmas: List['Lemma']
-    related_meanings: List[LazyType['Meaning', 'mpcd.dict.types.meaning']]
-    comments: List[LazyType['Comment', 'mpcd.corpus.types.comment']]
+    related_meanings: List[Annotated['Meaning', lazy('mpcd.dict.types.meaning')]]
+    comments: List[Annotated['Comment', lazy('mpcd.corpus.types.comment')]]
 
 
 @gql.django.input(models.Lemma)
--- mpcd/dict/types/meaning.py
+++ mpcd/dict/types/meaning.py
@@ -1,22 +1,25 @@
 
+from strawberry import lazy
 from strawberry_django_plus import gql
-from strawberry_django_plus.mutations import resolvers
 from strawberry_django_plus.gql import relay
-from strawberry.lazy_type import LazyType
-from typing import List, TYPE_CHECKING
+from typing import Annotated, List, TYPE_CHECKING
 from mpcd.dict import models
+
+if TYPE_CHECKING:
+    from mpcd.corpus.types.token import Token
+    from mpcd.corpus.types.comment import Comment
 
 
 @gql.django.type(models.Meaning)
 class Meaning(relay.Node):
 
-    token_meanings:  relay.Connection[LazyType['Token', 'mpcd.corpus.types.token']]
+    token_meanings:  relay.Connection[Annotated['Token', lazy('mpcd.corpus.types.token')]]
 
     id: gql.auto
     meaning: gql.auto
     language: gql.auto
     related_meanings: List['Meaning']
-    comments: List[LazyType['Comment', 'mpcd.corpus.types.comment']]
+    comments: List[Annotated['Comment', lazy('mpcd.corpus.types.comment')]]
 
 
 @gql.django.input(models.Meaning)
