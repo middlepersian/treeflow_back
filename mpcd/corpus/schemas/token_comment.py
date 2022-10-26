1c36b498fedb7f57b51f608ee404bd50e491e6cc
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-10-26 10:26:00 +0200

    lazylist deprecated

--- mpcd/corpus/schemas/token_comment.py
+++ mpcd/corpus/schemas/token_comment.py
@@ -2,7 +2,6 @@ from typing import Optional
 from strawberry_django_plus import gql
 from strawberry_django_plus.gql import relay
 from strawberry_django_plus.optimizer import DjangoOptimizerExtension
-from strawberry.lazy_type import LazyType
 from mpcd.corpus.types.token_comment import TokenComment, TokenCommentInput, TokenCommentPartial
 
 
