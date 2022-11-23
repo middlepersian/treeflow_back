d3e9fa44047ef887ec98d4ab0d93601bf32b91d3
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-11-23 15:19:19 +0100

    lemma custom mutations up

--- mpcd/dict/schemas/lemma.py
+++ mpcd/dict/schemas/lemma.py
@@ -18,5 +18,37 @@ class Mutation:
     update_lemma: Lemma = gql.django.update_mutation(LemmaPartial)
     delete_lemma: Lemma = gql.django.delete_mutation(gql.NodeInput)
 
+    @gql.django.mutation
+    def add_related_lemma_to_lemma(self, info, lemma: relay.GlobalID, related_lemma: relay.GlobalID,) -> Lemma:
+        current_lemma = lemma.resolve_node(info)
+        related_lemma = related_lemma.resolve_node(info)
+        current_lemma.related_lemmas.add(related_lemma)
+        current_lemma.save()
+        return lemma
 
-schema = gql.Schema(query=Query, mutation= Mutation, extensions=[DjangoOptimizerExtension])
+    @gql.django.mutation
+    def add_related_meaning_to_lemma(self, info, lemma: relay.GlobalID, related_meaning: relay.GlobalID,) -> Lemma:
+        current_lemma = lemma.resolve_node(info)
+        related_meaning = related_meaning.resolve_node(info)
+        current_lemma.related_meanings.add(related_meaning)
+        current_lemma.save()
+        return lemma
+
+    @gql.django.mutation
+    def remove_related_lemma_from_lemma(self, info, lemma: relay.GlobalID, related_lemma: relay.GlobalID,) -> Lemma:
+        current_lemma = lemma.resolve_node(info)
+        related_lemma = related_lemma.resolve_node(info)
+        current_lemma.related_lemmas.remove(related_lemma)
+        current_lemma.save()
+        return lemma
+
+    @gql.django.mutation
+    def remove_related_meaning_from_lemma(self, info, lemma: relay.GlobalID, related_meaning: relay.GlobalID,) -> Lemma:
+        current_lemma = lemma.resolve_node(info)
+        related_meaning = related_meaning.resolve_node(info)
+        current_lemma.related_meanings.remove(related_meaning)
+        current_lemma.save()
+        return lemma
+
+
+schema = gql.Schema(query=Query, mutation=Mutation, extensions=[DjangoOptimizerExtension])
