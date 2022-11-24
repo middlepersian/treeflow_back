a46371a0b0528afa73eefdbd58d382cb325787fd
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-11-24 11:08:43 +0100

    query language added

--- mpcd/schema.py
+++ mpcd/schema.py
@@ -6,6 +6,7 @@ from strawberry_django_jwt.middleware import AsyncJSONWebTokenMiddleware
 from strawberry_django_plus.optimizer import DjangoOptimizerExtension
 from mpcd.corpus.schemas import bibliography, codex_part, comment, corpus, dependency, facsimile, folio, line, morphological_annotation, section_type, section, sentence, source, text_sigle, text, token_comment, token, user
 from mpcd.dict.schemas import lemma, meaning
+from mpcd.dict.types import language
 
 
 @gql.type
@@ -31,6 +32,11 @@ class Query(
     lemma.Query,
     meaning.Query
 ):
+
+    @gql.field
+    def languages(self) -> language.Language:
+        return language.Language
+
     pass
 
 
@@ -57,16 +63,16 @@ class Mutation(
 ):
     pass
     # https://django-graphql-jwt.domake.io/relay.html
-    #token_auth = jwt_mutations.ObtainJSONWebTokenAsync.obtain
-    #verify_token = jwt_mutations.VerifyAsync.verify
-    #refresh_token = jwt_mutations.RefreshAsync.refresh
-    #delete_token_cookie = jwt_mutations.DeleteJSONWebTokenCookieAsync.delete_cookie
+    # token_auth = jwt_mutations.ObtainJSONWebTokenAsync.obtain
+    # verify_token = jwt_mutations.VerifyAsync.verify
+    # refresh_token = jwt_mutations.RefreshAsync.refresh
+    # delete_token_cookie = jwt_mutations.DeleteJSONWebTokenCookieAsync.delete_cookie
 
     # Long running refresh tokens
-    #revoke_token = graphql_jwt.relay.Revoke.Field()
+    # revoke_token = graphql_jwt.relay.Revoke.Field()
     # delete_refresh_token_cookie = \
     #    graphql_jwt.relay.DeleteRefreshTokenCookie.Field()
 
 
-    #schema = strawberry.Schema(query=Query, mutation=Mutation, extensions=[AsyncJSONWebTokenMiddleware,  DjangoOptimizerExtension])
+    # schema = strawberry.Schema(query=Query, mutation=Mutation, extensions=[AsyncJSONWebTokenMiddleware,  DjangoOptimizerExtension])
 schema = strawberry.Schema(query=Query, mutation=Mutation, extensions=[DjangoOptimizerExtension])
