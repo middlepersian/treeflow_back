811a932684a064305c333c6536709a3d1530ddc5
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-11-30 10:54:43 +0100

    jwt added

--- mpcd/schema.py
+++ mpcd/schema.py
@@ -41,6 +41,7 @@ class Query(
 
 
 @gql.type
+
 class Mutation(
     bibliography.Mutation,
     codex_part.Mutation,
@@ -61,17 +62,17 @@ class Mutation(
     lemma.Mutation,
     meaning.Mutation
 ):
-    pass
+    
     # https://django-graphql-jwt.domake.io/relay.html
-    # token_auth = jwt_mutations.ObtainJSONWebTokenAsync.obtain
-    # verify_token = jwt_mutations.VerifyAsync.verify
-    # refresh_token = jwt_mutations.RefreshAsync.refresh
-    # delete_token_cookie = jwt_mutations.DeleteJSONWebTokenCookieAsync.delete_cookie
+    token_auth = jwt_mutations.ObtainJSONWebTokenAsync.obtain
+    verify_token = jwt_mutations.VerifyAsync.verify
+    refresh_token = jwt_mutations.RefreshAsync.refresh
+    delete_token_cookie = jwt_mutations.DeleteJSONWebTokenCookieAsync.delete_cookie
 
     # Long running refresh tokens
-    # revoke_token = graphql_jwt.relay.Revoke.Field()
-    # delete_refresh_token_cookie = \
-    #    graphql_jwt.relay.DeleteRefreshTokenCookie.Field()
+    #revoke_token = graphql_jwt.relay.Revoke.Field()
+    #delete_refresh_token_cookie = \
+    #graphql_jwt.relay.DeleteRefreshTokenCookie.Field()
 
     # schema = strawberry.Schema(query=Query, mutation=Mutation, extensions=[AsyncJSONWebTokenMiddleware,  DjangoOptimizerExtension])
-schema = strawberry.Schema(query=Query, mutation=Mutation, extensions=[DjangoOptimizerExtension])
+schema = strawberry.Schema(query=Query, mutation=Mutation, extensions=[AsyncJSONWebTokenMiddleware, DjangoOptimizerExtension])
