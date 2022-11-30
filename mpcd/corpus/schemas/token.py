c2d4d37a829cf2ced191d4f30aa6f205f53bc63b
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-11-30 10:55:30 +0100

    login_required for token query

--- mpcd/corpus/schemas/token.py
+++ mpcd/corpus/schemas/token.py
@@ -1,22 +1,26 @@
-from typing import Optional, List
+from typing import Optional, List, cast
 from strawberry_django_plus import gql
 from strawberry_django_plus.gql import relay
 from strawberry_django_plus.optimizer import DjangoOptimizerExtension
 from strawberry.types.info import Info
+from strawberry_django_jwt.decorators import login_required
 
 
 from mpcd.corpus.types.token import Token, TokenInput, TokenPartial
+from mpcd.corpus.types.token_comment import TokenCommentInput
 import mpcd.corpus.models as models
 
 
 @gql.type
 class Query:
-    token: Optional[Token] = gql.django.node()
+    token: Optional[Token] = login_required(gql.django.node())
+    #tokens: relay.Connection[Token] = login_required(gql.django.connection())
     tokens: relay.Connection[Token] = gql.django.connection()
 
 
 @gql.type
 class Mutation:
+
     create_token: Token = gql.django.create_mutation(TokenInput)
     update_token: Token = gql.django.update_mutation(TokenPartial)
     delete_token: Token = gql.django.delete_mutation(gql.NodeInput)
@@ -66,6 +70,19 @@ class Mutation:
         token = token.resolve_node(info)
         lemmas = [lemma.resolve_node(info) for lemma in lemmas]
         token.lemmas.remove(*lemmas)
+        token.save()
+        return token
+
+    @gql.django.mutation
+    def add_token_comment_to_token(self,
+                                   info,
+                                   token: relay.GlobalID,
+                                   token_comment: TokenCommentInput,
+                                   ) -> Token:
+
+        token = token.resolve_node(info)
+        token_comment = models.TokenComment.objects.create(**token_comment)
+        token.comments.add(token_comment)
         token.save()
         return token
 
