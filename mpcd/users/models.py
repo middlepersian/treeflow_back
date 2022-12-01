a125953110dfc8202754922e8087daf9096d4cd5
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-12-01 09:49:17 +0100

    user model up

--- mpcd/users/models.py
+++ mpcd/users/models.py
@@ -9,8 +9,10 @@ class User(AbstractUser):
 
     #: First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
-    first_name = None  # type: ignore
-    last_name = None  # type: ignore
+    #first_name = None  # type: ignore
+    #last_name = None  # type: ignore
+    USERNAME_FIELD = "username"   # e.g: "username", "email"
+    EMAIL_FIELD = "email"         # e.g: "email", "primary_email"
 
     def get_absolute_url(self):
         """Get url for user's detail view.
