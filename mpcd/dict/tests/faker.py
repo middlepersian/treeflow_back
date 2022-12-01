e44304b7194fccc4cf203d0fc9c4520821951952
Francisco Mondaca <f.mondaca@uni-koeln.de>
2022-12-01 09:48:47 +0100

    enum tests init

---
+++ mpcd/dict/tests/faker.py
@@ -0,0 +1,41 @@
+from typing import Generic, List, Type, TypeVar, cast
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser, Group
+import factory
+from factory import fuzzy
+
+
+import mpcd.dict.models as models
+
+
+_T = TypeVar("_T")
+User = cast(Type[AbstractUser], get_user_model())
+
+
+class _BaseFactory(Generic[_T], factory.django.DjangoModelFactory):
+    @classmethod
+    def create(cls, **kwargs) -> _T:
+        return super().create(**kwargs)
+
+    @classmethod
+    def create_batch(cls, size: int, **kwargs) -> List[_T]:
+        return super().create_batch(size, **kwargs)
+
+
+class GroupFactory(_BaseFactory[Group]):
+    class Meta:
+        model = Group
+
+    name = factory.Sequence(lambda n: f"Group {n}")
+
+
+# Lemma
+
+class LemmaFactory(_BaseFactory[models.Lemma]):
+    class Meta:
+        model = Lemma
+
+    word = factory.Sequence(lambda n: f"Word {n}")
+    #language = factory.Sequence(lambda n: f"Language {n}")
--- mpcd/dict/types/lemma.py
+++ mpcd/dict/types/lemma.py
@@ -1,10 +1,12 @@
 
+import strawberry
+from enum import Enum
 from strawberry import lazy
 from strawberry_django_plus import gql
 from strawberry_django_plus.gql import relay
 from typing import List, TYPE_CHECKING, Annotated
 from mpcd.dict import models
-from mpcd.dict.types import language
+#from mpcd.dict.types.language import Language
 
 if TYPE_CHECKING:
     from mpcd.corpus.types.token import Token
@@ -12,32 +14,37 @@ if TYPE_CHECKING:
     from .meaning import Meaning
 
 
+@strawberry.enum
+class Language(Enum):
+    eng = 'eng'
+    deu = 'deu'
+    ita = 'ita'
+
+
 @gql.django.filters.filter(models.Lemma, lookups=True)
 class LemmaFilter:
     id: relay.GlobalID
     word: gql.auto
-    language : gql.auto
+    language: Language
 
 
 @gql.django.type(models.Lemma, filters=LemmaFilter)
 class Lemma(relay.Node):
 
-    token_lemmas:  relay.Connection[gql.LazyType['Token', 'mpcd.corpus.types.token']]
-
     id: relay.GlobalID
     word: gql.auto
-    language : gql.auto
+    language: Language
     related_lemmas: List['Lemma']
     related_meanings: List[gql.LazyType['Meaning', 'mpcd.dict.types.meaning']]
     comments: List[gql.LazyType['Comment', 'mpcd.corpus.types.comment']]
 
+    token_lemmas:  relay.Connection[gql.LazyType['Token', 'mpcd.corpus.types.token']]
 
 
 @gql.django.input(models.Lemma)
 class LemmaInput:
     word: gql.auto
-    language : gql.auto
-
+    language: Language
     related_lemmas: gql.auto
     related_meanings: gql.auto
 
@@ -46,7 +53,6 @@ class LemmaInput:
 class LemmaPartial:
     id: relay.GlobalID
     word: gql.auto
-    language : gql.auto
-
+    language: Language
     related_lemmas: gql.auto
     related_meanings: gql.auto
