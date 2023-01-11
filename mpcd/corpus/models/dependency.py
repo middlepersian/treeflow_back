7946e239d41338a5b336bbf88d6f07fc20433540
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-01-11 08:29:32 +0100

    comments rm

--- mpcd/corpus/models/dependency.py
+++ mpcd/corpus/models/dependency.py
@@ -10,7 +10,6 @@ class Dependency(models.Model):
     rel = models.CharField(max_length=9)
     # producer values: manual(1), computational(2). see schemas/dependency_enum.py
     producer = models.SmallIntegerField(null=True, blank=True)
-    #comments = models.ManyToManyField('Comment', blank=True, related_name = 'dependency_comments')
 
     history = HistoricalRecords()
 
