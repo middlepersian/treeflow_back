1df8b20c40f2660b805bd550b3d21461a1a48562
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-01-16 17:06:28 +0100

    factories up

--- mpcd/corpus/tests/factories/text.py
+++ mpcd/corpus/tests/factories/text.py
@@ -13,7 +13,47 @@ class TextFactory(factory.django.DjangoModelFactory):
     title = factory.Faker("pystr", min_chars=1, max_chars=30)
     text_sigle = factory.SubFactory(TextSigleFactory)
     stage = factory.Faker("pystr", max_chars=3  )
-    #sources = factory.SubFactory(SourceFactory)
-    #resources = factory.SubFactory(BibEntryFactory)
-    #editors = factory.SubFactory(UserFactory)
-    #collaborators = factory.SubFactory(UserFactory)
+
+    @factory.post_generation
+    def editors(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            for editor in extracted:
+                self.editors.add(editor)
+
+    @factory.post_generation
+    def authors(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            for author in extracted:
+                self.authors.add(author)
+
+    @factory.post_generation
+    def collaborators(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            for collaborator in extracted:
+                self.collaborators.add(collaborator)
+
+    @factory.post_generation
+    def sources(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            for source in extracted:
+                self.sources.add(source)
+
+    @factory.post_generation
+    def resources(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            for resource in extracted:
+                self.resources.add(resource)
+               
+               
+            
+             
