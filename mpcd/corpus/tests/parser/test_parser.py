6ab892e80c00f1486ebf0c5ed63effdc976fd684
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-01-30 17:44:40 +0100

    parse_annotated up

--- mpcd/corpus/tests/parser/test_parser.py
+++ mpcd/corpus/tests/parser/test_parser.py
@@ -148,7 +148,7 @@ def test_create_dmx():
 
 
 @pytest.mark.django_db
-def populate_db(sentences, text_object=None):
+def parse_preannotated(sentences, text_object=None):
 
     token_number = 1
     current_newpart = None
@@ -256,7 +256,10 @@ def populate_db(sentences, text_object=None):
 
 
 @pytest.mark.django_db
-def parse_dict(sentences, facsimile,  line_identifiers, folio_identifiers, text):
+def parse_annotated(sentences, text):
+
+    current_newpart = None
+    newparts = {}
 
     line_count = {}
     folio_count = {}
@@ -276,78 +279,107 @@ def parse_dict(sentences, facsimile,  line_identifiers, folio_identifiers, text)
     section_section_type = SectionTypeFactory(identifier="section")
 
     token_number = 1
-    for sentence in sentences:
-        for token_dict in sentence['tokens']:
-            try:
+    sentence_number = 1
 
-                line_identifier = str(token_dict['folio']) + '_' + \
-                    str(token_dict['line'])  # Initialize the line_id variable
-                folio_identifier = facsimile.bib_entry.key + '_' + \
-                    str(token_dict['folio'])  # Initialize the folio_id variable
+    for i, row in sentence.iterrows():
+        try:
 
-                # Get or create a Token object with the specified token ID
-                token_obj = TokenFactory(number=token_number, text=text,
-                                         transcription=token_dict['transcription'], transliteration=token_dict['transliteration'])
-                # check POS
-                if token_dict.get('pos'):
-                    token_obj.pos = token_dict['pos']
-                # check lemma
-                if token_dict.get('lemma'):
-                    lemma_obj = LemmaFactory(word=token_dict['lemma'], language='pah')
-                    token_obj.lemma = token_dict['lemma']
-                token_obj.lemmas.add(lemma_obj)
-                # split meaning
-                meanings = token_dict['meaning'].split(',')
-                for meaning in meanings:
-                    meaning_obj = MeaningFactory(meaning=meaning, language='eng')
-                    token_obj.meanings.add(meaning_obj)
-                token_obj.save()
+            # parse sentence info
+            #check if token_id starts with #
+            if str(transcription).strip().startswith("#"):
+                if str(transcription).startswith("#TRANSLATION"):
+                    # increase sentence number
+                    translation = str(transcription).split(":")[1].strip()
+                    assert translation != ""
+                elif str(transcription).startswith("#COMMENT"):
+                    comment = str(transcription).split(":")[1].strip()
+                    assert sentence_text != ""
+                # increase sentence number
+                sentence_number += 1
+                continue
 
-                line_identifiers.add(line_identifier)
-                folio_identifiers.add(folio_identifier)
+            line_identifier = str(row['line'])
+            folio_identifier = facsimile.bib_entry.key + '_' + \
+                str(token_dict['folioNew'])  # Initialize the folio_id variable
 
-                if line_identifier not in line_count:
-                    line_count[line_identifier] = 1
-                else:
-                    line_count[line_identifier] += 1
-                if folio_identifier not in folio_count:
-                    folio_count[folio_identifier] = 1
-                else:
-                    folio_count[folio_identifier] += 1
+            # Get or create a Token object with the specified token ID
+            token_obj = TokenFactory(number=token_number, text=text,
+                                     transcription=token_dict['transcription'], transliteration=token_dict['transliteration'])
+            # check POS
+            if row.get('POSTag'):
+                token_obj.pos = token_dict['POSTag']
+            # check lemma
+            if row.get('lemma'):
+                lemma_obj = LemmaFactory(word=row['lemma'], language='pah')
+                token_obj.lemma = token_dict['lemma']
+            token_obj.lemmas.add(lemma_obj)
+            # split meaning
+            meanings = token_dict['meaning'].split(',')
+            for meaning in meanings:
+                meaning_obj = MeaningFactory(meaning=meaning, language='eng')
+                token_obj.meanings.add(meaning_obj)
+            token_obj.save()
 
-                # Get or create a Line object with the specified line ID
-                line_obj = SectionFactory(section_type=line_section_type, identifier=line_identifier, text=text)
-                # Check if this is the first time the line has been encountered
-                if line_identifier in line_count and line_count[line_identifier] > 1:
-                    # Set the previous_line field to the previous Line object
-                    line_obj.previous = previous_line_obj
+            line_identifiers.add(line_identifier)
+            folio_identifiers.add(folio_identifier)
 
-                previous_line_obj = line_obj  # Update the previous Line object
-                # add token to line
-                line_obj.tokens.add(token_obj)
-                line_obj.save()
+            if line_identifier not in line_count:
+                line_count[line_identifier] = 1
+            else:
+                line_count[line_identifier] += 1
+            if folio_identifier not in folio_count:
+                folio_count[folio_identifier] = 1
+            else:
+                folio_count[folio_identifier] += 1
 
-                # Get or create a Folio object with the specified folio ID
-                # create the folio with factor_boy
-                folio_obj = FolioFactory(identifier=folio_identifier, facsimile=facsimile,
-                                         number=folio_count[folio_identifier])
-                assert folio_obj.identifier == folio_identifier
+            # Get or create a Line object with the specified line ID
+            line_obj = SectionFactory(section_type=line_section_type, identifier=line_identifier, text=text)
+            # Check if this is the first time the line has been encountered
+            if line_identifier in line_count and line_count[line_identifier] > 1:
+                # Set the previous_line field to the previous Line object
+                line_obj.previous = previous_line_obj
 
-                if folio_identifier in folio_count and folio_count[folio_identifier] > 1:
-                    # Set the previous_folio field to the previous Folio object
-                    folio_obj.previous = previous_folio_obj
-                previous_folio_obj = folio_obj  # Update the previous Folio object
-                folio_obj.sections.add(line_obj)
-                folio_obj.save()
+            previous_line_obj = line_obj  # Update the previous Line object
+            # add token to line
+            line_obj.tokens.add(token_obj)
+            line_obj.save()
 
-                # process sections
-                # sentence section
+            # Get or create a Folio object with the specified folio ID
+            # create the folio with factor_boy
+            folio_obj = FolioFactory(identifier=folio_identifier, facsimile=facsimile,
+                                     number=folio_count[folio_identifier])
+            assert folio_obj.identifier == folio_identifier
 
-                token_number += 1
-            except Exception as e:
-                print(e)
-                print(token_dict)
-                raise e
+            if folio_identifier in folio_count and folio_count[folio_identifier] > 1:
+                # Set the previous_folio field to the previous Folio object
+                folio_obj.previous = previous_folio_obj
+            previous_folio_obj = folio_obj  # Update the previous Folio object
+            folio_obj.sections.add(line_obj)
+            folio_obj.save()
+
+            # process sections
+            # if there is a newpart, add it to the newparts dictionary
+            if row["newpart"] == row["newpart"]:
+                # check if newpart is not a digit or a _
+                if str(row["newpart"]).strip():
+                    # check if not _ or digit
+                    if str(row["newpart"]).strip() != "_":
+                        if not str(row["newpart"]).strip().replace('.', '').isdigit():
+                            print("newpart: {} - sentence #{}".format(row["newpart"], sentence_id))
+
+                newpart = str(row["newpart"])
+                if newpart.strip() not in ["_", ""]:
+                    current_newpart = newpart
+                    token_newpart = current_newpart
+                    newpart_number += 1
+                    newparts[newpart_number] = token_newpart
+            assert current_newpart != ""
+            # increase token number
+            token_number += 1
+        except Exception as e:
+            print(e)
+            print(token_dict)
+            raise e
 
     print(f"Number of lines: {len(line_count)}, lines: {line_count}")
     print(f"Number of folios: {len(folio_count)}, folios: {folio_count}")
