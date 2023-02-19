c3bfab2fa5dd6b8bcd952c5d7f0650622cf3a3f9
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-02-19 16:22:21 +0100

    optimization init

--- treeflow/corpus/models/token.py
+++ treeflow/corpus/models/token.py
@@ -6,33 +6,31 @@ from simple_history.models import HistoricalRecords
 
 class Token(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid_lib.uuid4, editable=False)
-    number = models.FloatField(null=True, blank=True)
+    number = models.FloatField(null=True, blank=True,  db_index=True)
     number_in_sentence = models.FloatField(blank=True, null=True)
 
     root = models.BooleanField(default=False)
     word_token = models.BooleanField(default=True)
     visible = models.BooleanField(default=True)
 
-    text = models.ForeignKey('Text', on_delete=models.CASCADE, null=True, blank=True, related_name='token_text')
+    text = models.ForeignKey('Text', on_delete=models.CASCADE, null=True, blank=True, related_name='token_text', db_index=True)
 
     language = models.CharField(max_length=3, null=True, blank=True)
-    transcription = models.CharField(max_length=50)
-    transliteration = models.CharField(max_length=50, blank=True)
-    lemmas = models.ManyToManyField('dict.Lemma', blank=True, related_name='token_lemmas')
-    meanings = models.ManyToManyField('dict.Meaning', blank=True, related_name='token_meanings')
+    transcription = models.CharField(max_length=50,  db_index=True)
+    transliteration = models.CharField(max_length=50, blank=True,  db_index=True)
+    lemmas = models.ManyToManyField('dict.Lemma', blank=True, related_name='token_lemmas', through='TokenLemma', db_index=True)
+    meanings = models.ManyToManyField('dict.Meaning', blank=True, related_name='token_meanings', through='TokenMeaning', db_index=True)
     upos = models.CharField(max_length=8, null=True, blank=True)
     xpos = ArrayField(models.CharField(max_length=8), null=True, blank=True)
-    features = models.ManyToManyField(
-        'Feature', blank=True, related_name='token_features')
-    dependencies = models.ManyToManyField('Dependency', blank=True, related_name="token_dependencies")
-
+    features = models.ManyToManyField('Feature', blank=True, related_name='token_features', through='TokenFeature', db_index=True)
+    dependencies = models.ManyToManyField('Dependency', blank=True, related_name="token_dependencies", through='TokenDependency', db_index=True)
     avestan = models.TextField(null=True, blank=True)
 
     previous = models.OneToOneField('self',
                                     related_name='next',
                                     blank=True,
                                     null=True,
-                                    on_delete=models.SET_NULL)
+                                    on_delete=models.SET_NULL, db_index=True)
 
     gloss = models.TextField(blank=True, null=True)
 
@@ -55,3 +53,24 @@ class Token(models.Model):
 
     def __str__(self):
         return '{}'.format(self.transcription)
+
+
+
+class TokenLemma(models.Model):
+    token = models.ForeignKey(Token, on_delete=models.CASCADE)
+    lemma = models.ForeignKey('dict.Lemma', on_delete=models.CASCADE)
+    # other fields...
+
+class TokenMeaning(models.Model):
+    token = models.ForeignKey(Token, on_delete=models.CASCADE)
+    meaning = models.ForeignKey('dict.Meaning', on_delete=models.CASCADE)
+    # other fields...
+
+class TokenFeature(models.Model):
+    token = models.ForeignKey(Token, on_delete=models.CASCADE)
+    feature = models.ForeignKey('Feature', on_delete=models.CASCADE)
+    # other fields...
+
+class TokenDependency(models.Model):
+    token = models.ForeignKey(Token, on_delete=models.CASCADE)
+    dependency = models.ForeignKey('Dependency', on_delete=models.CASCADE)
