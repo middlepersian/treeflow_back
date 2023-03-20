f22eed033573ad1f7e3642292ad98221f06e395e
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-03-20 14:40:19 +0100

    user up

--- treeflow/corpus/types/user.py
+++ treeflow/corpus/types/user.py
@@ -2,11 +2,17 @@ from strawberry_django_plus import gql
 from strawberry_django_plus.gql import relay
 from django.contrib.auth import get_user_model
 
+from strawberry_django_plus.directives import SchemaDirectiveExtension
+
+from strawberry_django_plus.permissions import (
+    IsAuthenticated,
+     IsSuperuser,
+)
 
 @gql.django.type(get_user_model())
 class User(relay.Node):
-    id: relay.GlobalID
-    username: gql.auto
-    is_superuser: gql.auto
-    is_staff: gql.auto
-    email: gql.auto
+    id: relay.GlobalID = gql.field(directives=[IsSuperuser()])
+    username: gql.auto = gql.field(directives=[IsSuperuser()])
+    is_superuser: gql.auto = gql.field(directives=[IsSuperuser()])
+    is_staff: gql.auto = gql.field(directives=[IsSuperuser()])
+    email: gql.auto = gql.field(directives=[IsSuperuser()])
