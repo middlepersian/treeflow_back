ef2fa6ef1fa8a36f0bb2a18b41e2b332044c6a6a
Moritz Esser <moritz_esser@hotmail.de>
2023-03-22 15:36:04 +0100

    update production

--- config/settings/production.py
+++ config/settings/production.py
@@ -11,7 +11,7 @@ ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["app.treeflow.uni-koel
 # DATABASES
 # ------------------------------------------------------------------------------
 DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
-DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
+# DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
 
 # CACHES
