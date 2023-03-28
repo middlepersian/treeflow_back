78cedeec293c7ac1764410c39f7671ce041278d1
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-03-28 13:37:25 +0200

    delete text

---
+++ treeflow/datafeed/management/commands/delete_text.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from treeflow.corpus.models.text import Text
+
+class Command(BaseCommand):
+    help = 'Deletes a Text object based on its title.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('title', type=str, help='The title of the Text object to be deleted.')
+
+    def handle(self, *args, **options):
+        title = options['title']
+
+        # Find the Text object with the given title
+        try:
+            text = Text.objects.get(title=title)
+        except Text.DoesNotExist:
+            self.stdout.write(self.style.ERROR(f"Error: Text object with title '{title}' not found."))
+            return
+
+        # Delete the Text object
+        text.delete()
+
+        self.stdout.write(self.style.SUCCESS(f"Successfully deleted Text object with title '{title}'."))
