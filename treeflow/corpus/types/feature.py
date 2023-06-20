34442b5eb59f2f419d154a04b24c5273db6f5b54
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-06-20 15:55:51 +0200

    feats mv

--- treeflow/corpus/types/feature.py
+++ treeflow/corpus/types/feature.py
@@ -5,140 +5,14 @@ from dataclasses import dataclass
 from treeflow.corpus import models
 from treeflow.corpus.types.pos import POSInput
 from treeflow.corpus.enums.pos import UPOSValues
+from treeflow.corpus.enums.features import upos_feature_feature_value
 
 import strawberry
 
 
-upos_feature_feature_value = {
- 'ADJ': {
-  'Number': ('Plur',),
-  'Degree': ('Cmp', 'Sup'),
-  'NumType': ('Ord', 'Mult'),
-  'Subcat': ('Tran', 'Intr'),
-  'Tense': ('Pres', 'Past'),
-  'VerbForm': ('Part',),
-  'Foreign': ('Yes',),
-  'Hyph': ('Yes',),
-  'Typo': ('Yes',),
-  'Transc': ('Yes',)},
- 'ADP': {'AdpType': ('Prep', 'Post', 'Circ'),
-  'Foreign': ('Yes',),
-  'Hyph': ('Yes',),
-  'Typo': ('Yes',),
-  'Transc': ('Yes',)},
- 'ADV': {
-  'AdvType': ('Man', 'Loc', 'Tim', 'Deg'),
-  'NumType': ('Card', 'Mult', 'Ord'),
-  'Deixis': ('Prox', 'Remt'),
-  'Degree': ('Cmp', 'Sup'),
-  'Foreign': ('Yes',),
-  'Hyph': ('Yes',),
-  'Typo': ('Yes',),},
- 'DET': {
-  'Definite': ('Ind', 'Spec'),
-  'PronType': ('Dem', 'Ind', 'Tot', 'Int', 'Prs', 'Emp'),
-  'Deixis': ('Prox', 'Remt'),
-  'Person': ('1', '2', '3'),
-  'Number': ('Sing', 'Plur'),
-  'Poss': ('Yes',),
-  'Refl': ('Yes',),
-  'Foreign': ('Yes',),
-  'Hyph': ('Yes',),
-  'Typo': ('Yes',),
-  'Transc': ('Yes',)},
- 'PUNCT': {
-  'PunctType': ('Semi', 'Dash', 'Excl', 'Quot'),
-  'PunctSide': ('Ini', 'Fin'),
-  'Foreign': ('Yes',),
-  'Hyph': ('Yes',),
-  'Typo': ('Yes',),
-  'Transc': ('Yes',)},
- 'SCONJ': {
-  'PronType': ('Rel',),
-  'Foreign': ('Yes',),
-  'Hyph': ('Yes',),
-  'Typo': ('Yes',),
-  'Transc': ('Yes',)},
- 'NUM': {
-  'NumType': ('Card', 'Fract', 'Sets'),
-  'Number': ('Plur',),
-  'Foreign': ('Yes',),
-  'Hyph': ('Yes',),
-  'Typo': ('Yes',),
-  'Transc': ('Yes',)},
- 'PART': {
-  'PartType': ('Mod', 'Neg', 'Emp', 'Vbp'),
-  'Foreign': ('Yes',),
-  'Hyph': ('Yes',),
-  'Typo': ('Yes',),
-  'Transc': ('Yes',)},
- 'PRON': {'PronType': ('Prs', 'Dem', 'Emp', 'Int', 'Rel', 'Tot', 'Ind', 'Rcp', 'Neg', 'Exc'),
-  'Person': ('1', '2', '3'),
-  'Number': ('Sing', 'Plur'),
-  'Case': ('Nom', 'Acc'),
-  'Polite': ('Inform', 'Form', 'Elev', 'Humb'),
-  'Deixis': ('Prox', 'Remt'),
-  'Poss': ('Yes',), 
-  'Refl': ('Yes',),
-  'Foreign': ('Yes',),
-  'Hyph': ('Yes',),
-  'Typo': ('Yes',),
-  'Transc': ('Yes',)},
- 'NOUN': {
-  'Number': ('Sing', 'Plur'),
-  'Polite': ('Inform', 'Form', 'Elev', 'Humb'),
-  'Animacy': ('Hum', 'Nhum', 'Anim', 'Inan'),
-  'Gender': ('Fem', 'Masc'),
-  'Case': ('Nom', 'Acc'),
-  'Subcat': ('Tran', 'Intr'),
-  'VerbForm': ('Inf', 'Part', 'Vnoun'),
-  'Tense': ('Pres', 'Past'),
-  'Foreign': ('Yes',),
-  'Hyph': ('Yes',),
-  'Typo': ('Yes',),
-  'Transc': ('Yes',)},
- 'PROPN': {'NameType': ('Giv', 'Pat', 'Geo', 'Oth'),
-  'Animacy': ('Hum', 'Nhum', 'Anim', 'Inan'),
-  'Gender': ('Fem', 'Masc'),
-  'Transc': ('Yes',),
-  'Foreign': ('Yes',),
-  'Hyph': ('Yes',),
-  'Typo': ('Yes',)},
- 'SYM': {
-  'Foreign': ('Yes',),
-  'Hyph': ('Yes',),
-  'Typo': ('Yes',),
-  'Transc': ('Yes',)},
- 'AUX': {'VerbType': ('Cop', 'Quasi', 'Light', 'Mod'),
-  'Subcat': ('Tran', 'Intr'),
-  'Tense': ('Pres', 'Past'),
-  'Mood': ('Ind', 'Sub', 'Opt', 'Imp', 'Nec'),
-  'Person': ('1', '2', '3'),
-  'Number': ('Sing', 'Plur'),
-  'VerbForm': ('Fin', 'Inf', 'Part', 'Conv', 'Vnoun'),
-  'Polarity': ('Neg',),
-  'Polite': ('Inform', 'Form', 'Elev', 'Humb'),
-  'Foreign': ('Yes',),
-  'Hyph': ('Yes',),
-  'Typo': ('Yes',),
-  'Transc': ('Yes',)},
- 'VERB': {'VerbType': ('Cop', 'Quasi', 'Light', 'Mod'),
-  'Subcat': ('Tran', 'Intr'),
-  'Tense': ('Pres', 'Past'),
-  'Mood': ('Ind', 'Sub', 'Opt', 'Imp', 'Nec'),
-  'Person': ('1', '2', '3'),
-  'Number': ('Sing', 'Plur'),
-  'VerbForm': ('Fin', 'Inf', 'Part', 'Conv', 'Vnoun'),
-  'Polite': ('Inform', 'Form', 'Elev', 'Humb'),
-  'Foreign': ('Yes',),
-  'Hyph': ('Yes',),
-  'Typo': ('Yes',),
-  'Transc': ('Yes',)}
-}
 
 def get_features(pos: str ) -> Dict[str, Tuple[str]]:
-    pos_features = upos_feature_feature_value
-    return pos_features.get(pos, {})
+    return upos_feature_feature_value.get(pos, {})
 
 @strawberry.type
 class UPOSList:
