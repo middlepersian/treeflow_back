0c81bff7de1fb3577b0331fee19393227e343675
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-07-18 11:56:20 +0200

    get_section_types_in_text added

--- treeflow/corpus/schemas/text.py
+++ treeflow/corpus/schemas/text.py
@@ -1,8 +1,12 @@
 from strawberry_django_plus import gql
 from strawberry_django_plus.gql import relay
 from strawberry_django_plus.optimizer import DjangoOptimizerExtension
-from typing import Optional
+from typing import Optional, List
+from asgiref.sync import sync_to_async
+
 from treeflow.corpus.types.text import Text, TextInput, TextPartial
+from treeflow.corpus.models.text import Text as TextModel
+from treeflow.corpus.models.section import Section as SectionModel
 
 from strawberry_django_plus.directives import SchemaDirectiveExtension
 
@@ -17,8 +21,25 @@ from strawberry_django_plus.permissions import (
 @gql.type
 class Query:
     text: Optional[Text] = gql.django.node()
-    texts:  relay.Connection[Text] = gql.django.connection()
+    texts: relay.Connection[Text] = gql.django.connection()
 
+    @gql.field
+    @sync_to_async
+    def get_section_types_in_text(self, info, text_id: relay.GlobalID) -> List[str]:
+
+        # Get the text object
+        text = text_id.resolve_node(info)
+
+        # Get all related sections
+        sections = SectionModel.objects.filter(text=text)
+
+        # Get all types
+        section_types = sections.values_list('type', flat=True)
+
+        # Make the section types unique by converting them to a set
+        unique_section_types = list(set(section_types))
+
+        return unique_section_types
 
 @gql.type
 class Mutation:
