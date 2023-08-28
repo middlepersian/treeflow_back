f3a8063b78bf12060e9b3f32712bb36eac1756d5
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-08-28 10:25:44 +0200

    NodeID[int]

--- treeflow/corpus/types/user.py
+++ treeflow/corpus/types/user.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 
 @strawberry_django.type(get_user_model())
 class User(relay.Node):
-    id: relay.NodeID[str]
+    id: relay.NodeID[int]
     username: strawberry.auto 
     is_superuser: strawberry.auto 
     is_staff: strawberry.auto 
