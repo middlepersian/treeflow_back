ebae4c4ae96aba4814c44ad08a5035bc8974ea52
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-11-08 16:09:59 +0100

    testing

--- treeflow/corpus/models/token.py
+++ treeflow/corpus/models/token.py
@@ -1,10 +1,13 @@
 import uuid as uuid_lib
 from django.db import models
+from django.db.models import Q, F
 from django.contrib.postgres.fields import ArrayField
 from simple_history.models import HistoricalRecords
 from treeflow.utils.normalize import strip_and_normalize
+from django.db import transaction
 
 class Token(models.Model):
+
     id = models.UUIDField(primary_key=True, default=uuid_lib.uuid4, editable=False)
     number = models.FloatField(null=True, blank=True)
     number_in_sentence = models.FloatField(blank=True, null=True)
@@ -81,8 +84,93 @@ class Token(models.Model):
         #save
         super().save(*args, **kwargs)
 
+    MIN_GAP = 0.01  # Define a class-level constant for the minimum gap    
 
+    @staticmethod
+    def find_nearest_tokens(token_number, text_id):
+        previous_token = Token.objects.filter(number__lt=token_number, text_id=text_id).order_by('-number').first()
+        next_token = Token.objects.filter(number__gt=token_number, text_id=text_id).order_by('number').first()
+        return previous_token, next_token
 
+    @staticmethod
+    def calculate_insert_number(before_number, after_number):
+        # Assuming we have a standard gap we observe, e.g., 0.1
+        # The logic here finds the midway point between two numbers,
+        # but you could implement a different logic that suits your needs.
+        return (before_number + after_number) / 2.0
+
+    @classmethod
+    def insert_before(cls, reference_token_id, new_token_data):
+        with transaction.atomic():
+            reference_token = cls.objects.select_for_update().get(id=reference_token_id)
+            text_id = reference_token.text_id
+            previous_token, next_token = cls.find_nearest_tokens(reference_token.number, text_id)
+            before_number = previous_token.number if previous_token else (reference_token.number - cls.MIN_GAP)
+            after_number = reference_token.number
+            new_number = cls.calculate_insert_number(before_number, after_number)
+
+            # Create a new token with the determined number and other data
+            new_token = cls(number=new_number, **new_token_data)
+            new_token.text_id = text_id
+            new_token.save()
+
+            # Update the 'next' field of the previous token to point to the new token, if applicable
+            if previous_token:
+                previous_token.next = new_token
+                previous_token.save()
+
+            # Update the 'previous' field of the reference token to point to the new token
+            reference_token.previous = new_token
+            reference_token.save()
+
+            # Update the 'next' field of the new token to point to the reference token
+            new_token.next = reference_token
+            new_token.save()
+
+            # Update the 'previous' field of the next token to point to the new token, if applicable
+            if next_token and not next_token.previous_id:
+                next_token.previous = new_token
+                next_token.save()
+
+            return new_token
+            
+    @classmethod
+    def insert_after(cls, reference_token_id, new_token_data):
+        with transaction.atomic():
+            reference_token = cls.objects.select_for_update().get(id=reference_token_id)
+            text_id = reference_token.text_id
+            _, next_token = cls.find_nearest_tokens(reference_token.number, text_id)
+            before_number = reference_token.number
+            after_number = next_token.number if next_token else (reference_token.number + cls.MIN_GAP)
+            new_number = cls.calculate_insert_number(before_number, after_number)
+
+            # Create a new token with the determined number and other data
+            new_token = cls(number=new_number, **new_token_data)
+            new_token.text_id = text_id
+            new_token.save()
+
+            # Update the 'previous' field of the new token to point to the reference token
+            if not cls.objects.filter(previous=reference_token).exists():
+                new_token.previous = reference_token
+                new_token.save()
+
+            # Update the 'next' field of the reference token to point to the new token
+            if reference_token and not reference_token.next:
+                reference_token.next = new_token
+                reference_token.save()
+                print(f"After updating reference_token.next: {reference_token.next}")
+                
+            # Update the 'previous' field of the next token to point to the new token, if applicable
+            if next_token and not cls.objects.filter(previous=new_token).exists():
+                next_token.previous = new_token
+                next_token.save()
+
+            # Update the 'next' field of the new token to point to the next token, if applicable
+            if next_token and not new_token.next:
+                new_token.next = next_token
+                new_token.save()
+
+            return new_token
 class TokenLemma(models.Model):
     token = models.ForeignKey(Token, on_delete=models.CASCADE)
     lemma = models.ForeignKey('dict.Lemma', on_delete=models.CASCADE)
---
+++ treeflow/corpus/tests/factories/test_token.py
@@ -0,0 +1,150 @@
+import pytest
+from treeflow.corpus.models import Token
+from treeflow.corpus.tests.factories import TokenFactory, TextFactory
+
+
+@pytest.mark.django_db
+def test_token_factory():
+    """Test token factory"""
+    token = TokenFactory()
+    assert isinstance(token, Token)
+
+    assert token.number
+    assert token.root == False
+    assert token.word_token
+    assert token.visible
+    assert token.text
+    assert token.language
+    assert token.transcription
+    assert token.transliteration
+    assert token.avestan
+    assert token.gloss
+
+
+    previous = TokenFactory()
+    token.previous = previous
+    assert isinstance(token.previous, Token)
+
+
+
+@pytest.mark.django_db
+def test_insert_token_logic():
+    # Create a text to associate tokens with
+    text = TextFactory() # Assuming you have a factory for creating Text instances
+
+    # Create a sequence of tokens with known numbers
+    token1 = TokenFactory(text=text, number=1.0)
+    token2 = TokenFactory(text=text, number=2.0)
+    token3 = TokenFactory(text=text, number=3.0)
+
+    # Link tokens manually or by some setup logic
+    token1.next = token2
+    token2.previous = token1
+    token2.next = token3
+    token3.previous = token2
+    token1.save()
+    token2.save()
+    token3.save()
+
+
+@pytest.mark.django_db
+def test_next_previous():
+    # Create a text to associate tokens with
+    text = TextFactory()
+
+    # Create a sequence of tokens with known numbers
+    token1 = TokenFactory(text=text, number=1.0)
+    token2 = TokenFactory(text=text, number=2.0)
+    token3 = TokenFactory(text=text, number=3.0)
+
+    # Link tokens manually or by some setup logic
+    token1.next = token2
+    token2.previous = token1
+    token2.next = token3
+    token3.previous = token2
+    token1.save()
+    token2.save()
+    token3.save()
+
+    # Check that the tokens were linked correctly
+    assert token1.next == token2
+    assert token2.previous == token1
+    assert token2.next == token3
+    assert token3.previous == token2
+
+
+@pytest.mark.django_db
+def test_insert_before():
+    # Create a text to associate tokens with
+    text = TextFactory()
+
+    # Create a sequence of tokens with known numbers
+    token1 = TokenFactory(text=text, number=1.0)
+    token2 = TokenFactory(text=text, number=2.0)
+    token3 = TokenFactory(text=text, number=3.0)
+
+    # Link tokens manually or by some setup logic
+    token1.next = token2
+    token2.previous = token1
+    token2.next = token3
+    token3.previous = token2
+    token1.save()
+    token2.save()
+    token3.save()
+
+    # Insert a new token before token2
+    new_token_data = {'transcription': 'new_token'}
+    new_token = Token.insert_before(token2.id, new_token_data)
+
+    # Reload tokens from the database
+    token1.refresh_from_db()
+    token2.refresh_from_db()
+    token3.refresh_from_db()
+
+    # Check that the tokens were linked correctly
+    assert token1.next == new_token
+    assert new_token.previous == token1
+    assert new_token.next == token2
+    assert token2.previous == new_token
+    assert token2.next == token3
+    assert token3.previous == token2
+    #print out the numbers
+    print(token1.number)
+    print(new_token.number)
+    print(token2.number)
+    print(token3.number)
+
+
+@pytest.mark.django_db
+def test_insert_after():
+    # Create a text to associate tokens with
+    text = TextFactory()
+
+    # Create a sequence of tokens with known numbers
+    token1 = TokenFactory(text=text, number=1.0)
+    token2 = TokenFactory(text=text, number=2.0)
+    token3 = TokenFactory(text=text, number=3.0)
+
+    # Link tokens manually or by some setup logic
+    token1.next = token2
+    token2.previous = token1
+    token2.next = token3
+    token3.previous = token2
+    token1.save()
+    token2.save()
+    token3.save()
+
+    #Insert a new token after token2
+    new_token_data = {'transcription': 'new_token'}
+    new_token = Token.insert_after(token2.id, new_token_data)
+
+    # Print tokens before refreshing
+    print(f"Before refreshing: token2.next={token2.next}, new_token.previous={new_token.previous}, new_token.next={new_token.next}")
+
+    # Reload tokens from the database
+    token1.refresh_from_db()
+    token2.refresh_from_db()
+    token3.refresh_from_db()
+
+    # Print tokens after refreshing
+    print(f"After refreshing: token2.next={token2.next}, new_token.previous={new_token.previous}, new_token.next={new_token.next}")
