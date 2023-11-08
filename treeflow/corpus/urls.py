c550d18801ba4a5e633bca488a0a234d688df719
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-11-08 18:28:08 +0100

    insert tokens buttons

--- treeflow/corpus/urls.py
+++ treeflow/corpus/urls.py
@@ -1,8 +1,12 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
+
 from treeflow.corpus.views.update_token import update_token
 from treeflow.corpus.views.sections import sections_view
 from treeflow.corpus.views.ud_editor import ud_editor
 from treeflow.corpus.views.tokens import tokens_view
+from treeflow.corpus.views.insert_after_token import insert_after_token_view
+from treeflow.corpus.views.insert_before_token import insert_before_token_view
+
 
 app_name = "treeflow.corpus"
 urlpatterns = [
@@ -10,4 +14,5 @@ urlpatterns = [
     path('sections/', sections_view, name='sections_view'),
     path('ud-editor/<uuid:section_id>/', ud_editor, name='ud_editor'), 
     path('tokens/', tokens_view, name='tokens'),
-]
+    re_path(r'^tokens/(?P<token_id>[0-9a-f-]+)/insert_after/$', insert_after_token_view, name='insert_after_token'),
+    re_path(r'^tokens/(?P<token_id>[0-9a-f-]+)/insert_before/$', insert_before_token_view, name='insert_before_token'),]
