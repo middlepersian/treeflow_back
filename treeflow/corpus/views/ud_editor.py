9a27b22ce33a1f9233ec9e9f7731460dd26be975
Moritz Esser <moritz.esser@uni-koeln.de>
2023-11-09 11:52:20 +0100

    ud

--- treeflow/corpus/views/ud_editor.py
+++ treeflow/corpus/views/ud_editor.py
@@ -8,28 +8,32 @@ from treeflow.corpus.models import (
 )  # Adjust to your model's import path
 from django.db.models import Q
 
+def calculate_positions(word_length_1, x1, font_size, gap):
+    char_width = font_size * 0.6
+    word_width_1 = word_length_1 * char_width
+
+    x2 = x1 + word_width_1 + gap
+
+    return x1, x2
+
 
 def ud_editor(request, section_id):
-    # Get the section by ID and ensure it's of type 'sentence'
     section = get_object_or_404(Section, pk=section_id, type="sentence")
-
-    # Get all SectionToken instances for the section
     section_tokens = section.tokens.all()
-
-    # Extract token instances from section_tokens
     tokens = list(section_tokens)
 
-    # Now get all dependencies for those tokens
     dependencies = (
         Dependency.objects.filter(Q(token__in=tokens) | Q(head__in=tokens))
         .select_related("token", "head")
         .distinct()
     )
 
-    # Prepare token and dependency data
     token_data = []
+    x1 = 0
     for token in tokens:
         token_dependencies = []
+        x1, x2 = calculate_positions(len(token.transcription), x1, 12, 10)
+
         for dep in dependencies:
             if dep.token == token:
                 token_dependencies.append(
@@ -38,10 +42,10 @@ def ud_editor(request, section_id):
                         "from_token_id": {
                             "id": dep.token.id,
                             "transcription": dep.token.transcription,
-                            "xpos": dep.token.number_in_sentence * 50
+                            "xpos": x1
                             if dep.token.number_in_sentence
                             else 0,
-                            "ypos": dep.token.number_in_sentence * 50
+                            "ypos": 50
                             if dep.token.number_in_sentence
                             else 0,
                         },
@@ -59,10 +63,10 @@ def ud_editor(request, section_id):
             {
                 "id": token.id,
                 "number": token.number_in_sentence,
-                "xpos": token.number_in_sentence * 50
+                "xpos": x1
                 if token.number_in_sentence
                 else 0,
-                "ypos": token.number_in_sentence * 50
+                "ypos": 50
                 if token.number_in_sentence
                 else 0,
                 "transcription": token.transcription,
@@ -70,6 +74,7 @@ def ud_editor(request, section_id):
                 "dependencies": token_dependencies,
             }
         )
+        x1 = x2
 
     # Prepare context for the template
     context = {
