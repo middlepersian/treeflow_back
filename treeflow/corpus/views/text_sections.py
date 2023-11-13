0c731836bb1a2974a9fae74481eba5cb108333fd
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-11-13 13:48:19 +0100

    section_view

---
+++ treeflow/corpus/views/text_sections.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.db.models import Prefetch
+from treeflow.corpus.models import Section, Token
+
+from django.db.models import Prefetch
+
+def text_sections_view(request, text_id):
+    # Define the queryset for tokens that prefetches the related section tokens
+    # Filter the tokens through the 'SectionToken' intermediary table
+    token_queryset = Token.objects.filter(
+        sectiontoken__section__text_id=text_id,
+        sectiontoken__section__type='sentence'
+    ).order_by('sectiontoken__section__number', 'number_in_sentence')
+
+    # Create a Prefetch object specifically for 'section_tokens' related to 'sentence' type sections
+    # The 'to_attr' parameter specifies the attribute name where the prefetched tokens will be stored
+    tokens_prefetch = Prefetch(
+        'tokens', 
+        queryset=token_queryset,
+        to_attr='prefetched_tokens'
+    )
+
+    # Fetch sections of type 'sentence' for the given text, using the Prefetch object
+    sections = Section.objects.filter(
+        text__id=text_id, 
+        type='sentence'
+    ).prefetch_related(tokens_prefetch).order_by('number')
+
+    # Pass the sentence_sections with prefetched tokens to the template
+    context = {
+        'sections': sections,
+    }
+
+    return render(request, 'text_sections.html', context)
