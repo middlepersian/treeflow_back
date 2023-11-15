6ee14ae52fb98817815052e2597a688c43f1dba2
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-11-15 11:13:28 +0100

    ajax in tha house

--- treeflow/corpus/urls.py
+++ treeflow/corpus/urls.py
@@ -9,6 +9,7 @@ from treeflow.corpus.views.insert_before_token import insert_before_token_view
 from treeflow.corpus.views.delete_token import delete_token_view
 from treeflow.corpus.views.texts import texts_view
 from treeflow.corpus.views.sections_editor import sections_editor_view
+from treeflow.corpus.views.get_sections import get_sections_by_type
 
 app_name = "treeflow.corpus"
 urlpatterns = [
@@ -18,5 +19,6 @@ urlpatterns = [
     path('tokens/', tokens_view, name='tokens'),
     path('texts/', texts_view, name='texts'),
     path('texts/<uuid:text_id>/sections/', sections_editor_view, name='sections-editor-view'),
+    path('get-sections/<uuid:text_id>/<str:section_type>/', get_sections_by_type, name='get_sections_by_type'),
     re_path(r'^tokens/(?P<token_id>[0-9a-f-]+)/insert_after/$', insert_after_token_view, name='insert_after_token'),
     re_path(r'^tokens/(?P<token_id>[0-9a-f-]+)/insert_before/$', insert_before_token_view, name='insert_before_token'),]
