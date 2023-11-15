da048ea385aa26cddce01046600cc0c1fae3bc3f
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-11-15 09:29:23 +0100

    tokens up

--- treeflow/corpus/urls.py
+++ treeflow/corpus/urls.py
@@ -6,6 +6,9 @@ from treeflow.corpus.views.ud_editor import ud_editor
 from treeflow.corpus.views.tokens import tokens_view
 from treeflow.corpus.views.insert_after_token import insert_after_token_view
 from treeflow.corpus.views.insert_before_token import insert_before_token_view
+from treeflow.corpus.views.delete_token import delete_token_view
+from treeflow.corpus.views.texts import texts_view
+from treeflow.corpus.views.update_pos_ajax import update_pos_ajax
 
 
 app_name = "treeflow.corpus"
@@ -14,5 +17,7 @@ urlpatterns = [
     path('sections/', sections_view, name='sections_view'),
     path('ud-editor/<uuid:section_id>/', ud_editor, name='ud_editor'), 
     path('tokens/', tokens_view, name='tokens'),
+    path('texts/', texts_view, name='texts'),
+    path('ajax/update_pos/', update_pos_ajax, name='update_pos_ajax'),  # The AJAX endpoint for updating POS
     re_path(r'^tokens/(?P<token_id>[0-9a-f-]+)/insert_after/$', insert_after_token_view, name='insert_after_token'),
     re_path(r'^tokens/(?P<token_id>[0-9a-f-]+)/insert_before/$', insert_before_token_view, name='insert_before_token'),]
