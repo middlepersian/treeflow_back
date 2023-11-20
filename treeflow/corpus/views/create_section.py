c3ccb06449ee29a554ec30aaa00ab7ef7e0eaf73
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-11-20 17:01:25 +0100

    test wip

--- treeflow/corpus/views/create_section.py
+++ treeflow/corpus/views/create_section.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from treeflow.corpus.forms.section_form import SectionForm
-from treeflow.corpus.models import Token
+from treeflow.corpus.models.token import Token
 from django.http import JsonResponse
 import logging
 import uuid
