9c614cdd64cb327658ef1dbcf2cdbb0f670857be
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-11-24 13:39:04 +0100

    pos select

--- treeflow/corpus/forms/pos_form.py
+++ treeflow/corpus/forms/pos_form.py
@@ -2,15 +2,18 @@
 from django import forms
 from django.forms import inlineformset_factory
 from treeflow.corpus.models import POS, Token
-
-# POSForm for individual POS entries
 class POSForm(forms.ModelForm):
     class Meta:
         model = POS
-        fields = ('pos', 'type')  # or any other fields you need
+        fields = ('pos', 'type')
+
+    def __init__(self, *args, **kwargs):
+        super(POSForm, self).__init__(*args, **kwargs)
+        pos_choices = POS.objects.order_by('pos').values_list('pos', flat=True).distinct()
+        self.fields['pos'].choices = [(pos, pos) for pos in pos_choices]
 
 # POSFormSet to manage a collection of POS forms
 POSFormSet = inlineformset_factory(
     Token, POS, form=POSForm, 
-    fields=('pos', 'type'), extra=1, can_delete=True
+    fields=('pos', 'type'), extra=0, can_delete=True
 )
--- treeflow/corpus/views/tokens.py
+++ treeflow/corpus/views/tokens.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from treeflow.corpus.models import Text, Token, POS
+from django.db.models import Prefetch
 
 
 def tokens_view(request):
     # Get all Text objects for the dropdowns
     texts = Text.objects.all()
-    # get all pos choices    
+    # Get all pos choices
     pos_choices = POS.objects.order_by('pos').values_list('pos', flat=True).distinct()
 
     # Retrieve GET parameters
     selected_text_id = request.GET.get('text_id')
 
-    # Start with an initial tokens queryset
-    # and prefetch related data to minimize database hits
+    # Start with an initial tokens queryset and prefetch related data
     tokens = Token.objects.prefetch_related(
         'lemmas',  # Prefetch related Lemma objects
         'senses',  # Prefetch related Senses objects
+        'pos_token'  # Prefetch related POS objects
     )
 
     # Filter tokens if a text ID is provided
@@ -24,7 +25,7 @@ def tokens_view(request):
         tokens = tokens.filter(text__id=selected_text_id)
 
     # Setup paginator
-    paginator = Paginator(tokens, 100)  # Show 10 tokens per page
+    paginator = Paginator(tokens, 100)  # Show 100 tokens per page
     page_number = request.GET.get('page')
     tokens_page = paginator.get_page(page_number)
 
