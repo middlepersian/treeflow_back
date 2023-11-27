682e9fce0c39ef97dbbe629324bc1985746c1f1c
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-11-27 13:48:52 +0100

    scroll to top

--- treeflow/static/corpus/section_navigator.js
+++ treeflow/static/corpus/section_navigator.js
@@ -1,17 +1,3 @@
-// Scroll to top button
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
-
-window.addEventListener('scroll', function () {
-    const scrollToTopButton = document.querySelector('.scroll-to-top');
-    if (window.scrollY > 200) {
-        scrollToTopButton.classList.remove('hidden');
-    } else {
-        scrollToTopButton.classList.add('hidden');
-    }
-});
-
 // Highlight tokens when a section is clicked
 document.addEventListener('DOMContentLoaded', function () {
     const selector = document.getElementById('sectionTypeSelector');
