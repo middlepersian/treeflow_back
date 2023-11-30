39ddf56e685e2780dd33a9411c08f5fd4e908f6a
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-11-30 14:27:30 +0100

    sentences up

--- treeflow/corpus/views/sentences.py
+++ treeflow/corpus/views/sentences.py
@@ -1,6 +1,8 @@
+from treeflow.corpus.models import Text, Section, Token, SectionToken
+from django.core.paginator import Paginator, EmptyPage
+from django.core.cache import cache
 from django.shortcuts import render
-from django.core.paginator import Paginator
-from treeflow.corpus.models import Text, Section
+from django.db.models import Prefetch
 import logging
 
 logger = logging.getLogger(__name__)
@@ -10,38 +12,50 @@ def sentences_view(request):
     texts = Text.objects.all()
     # Retrieve GET parameters
     selected_text_id = request.GET.get('text_id')
-    page_number = int(request.GET.get('page', 1))
 
-    # Define the number of sentences per page
-    sentences_per_page = 10
 
-    # Get the total count of sentences
-    total_sentences_count = Section.objects.filter(type='sentence', text__id=selected_text_id).count()
+    # Retrieve all sentences (sections of type 'sentence')
+    sentences = Section.objects.filter(type='sentence')
+    # Prefetch for sections of type 'sentence'
+    sentence_prefetch = Prefetch(
+        'sectiontoken_set',
+        queryset=SectionToken.objects.filter(section__type='sentence').select_related('section'),
+        to_attr='sentence_sections'
+    )
 
-    # Setup paginator with the total count
-    paginator = Paginator(range(total_sentences_count), sentences_per_page)
-    sentences_page = paginator.page(page_number)
+    # Prefetch for sections of type 'line'
+    line_prefetch = Prefetch(
+        'sectiontoken_set',
+        queryset=SectionToken.objects.filter(section__type='line').select_related('section'),
+        to_attr='line_sections'
+    )
+    # If a text ID is provided, filter sentences by this text
+    if selected_text_id:
+        sentences = sentences.filter(text__id=selected_text_id)
 
-    # Calculate the starting and ending points for the query
-    start = (page_number - 1) * sentences_per_page
-    end = start + sentences_per_page
+    # Setup paginator for sentences
+    paginator = Paginator(sentences, 10)  # Adjust the number of sentences per page as needed
+    page_number = request.GET.get('page')
+    sentences_page = paginator.get_page(page_number)
 
-    # Fetch only the sentences for the current page
-    sentences = Section.objects.filter(type='sentence', text__id=selected_text_id).only('type', 'text', 'id', 'identifier').prefetch_related(
-        'sectiontoken_set__token__lemmas',
-        'sectiontoken_set__token__senses',
-        'sectiontoken_set__token__pos_token'
-    )[start:end]
-
-    logger.debug(sentences.query)
-    logger.debug(len(sentences))
+    # Retrieve tokens for each sentence in the current page
+    tokens_by_sentence = []
+    for sentence in sentences_page:
+        tokens = Token.objects.filter(
+            sectiontoken__section=sentence
+        ).prefetch_related(
+            'lemmas', 'senses', 'pos_token', 'feature_token',  # Prefetch related objects
+            sentence_prefetch, line_prefetch
+        )
+        tokens_by_sentence.append((sentence, tokens))
 
     # Prepare context for rendering
     context = {
         'texts': texts,
-        'sentences': sentences,
-        'page_obj': sentences_page,
+        'sentences_with_tokens': tokens_by_sentence,
         'selected_text_id': selected_text_id or '',
+        'page_obj': sentences_page,  # Include the page object for pagination controls
     }
 
+        # Render response
     return render(request, 'sentences.html', context)
