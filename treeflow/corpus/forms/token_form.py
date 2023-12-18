cc9741f15bc0a43e5e41aa93f1f8101354f19022
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-12-18 11:25:24 +0100

    wip

--- treeflow/corpus/forms/token_form.py
+++ treeflow/corpus/forms/token_form.py
@@ -1,26 +1,42 @@
-# forms.py
 from django import forms
 from treeflow.corpus.models.token import Token
 from treeflow.dict.models import Lemma, Sense
+from django_select2 import forms as s2forms
+
+class LemmaWidget(s2forms.ModelSelect2MultipleWidget):
+    search_fields = [
+        "lemma__icontains",  # Adjust the field name as per your Lemma model
+    ]
+
+class SenseWidget(s2forms.ModelSelect2MultipleWidget):
+    search_fields = [
+        "sense__icontains",
+    ]
 
 class TokenForm(forms.ModelForm):
     lemmas = forms.ModelMultipleChoiceField(
-        queryset=Lemma.objects.none(),  # Default to none, will be set in __init__
-        required=False
+        queryset=Lemma.objects.none(),
+        required=False,
+        widget=LemmaWidget,  # Use the custom LemmaWidget here
     )
     senses = forms.ModelMultipleChoiceField(
-        queryset=Sense.objects.none(),  # Default to none, will be set in __init__
-        required=False
+        queryset=Sense.objects.none(),
+        required=False,
+        widget=SenseWidget,  # Use the custom SenseWidget here
     )
 
     class Meta:
         model = Token
         fields = ['transcription', 'transliteration', 'lemmas', 'senses']
+        widgets = {
+            'lemmas': LemmaWidget,  # Assign LemmaWidget to 'lemmas' field
+            'senses': SenseWidget,  # Assign SenseWidget to 'senses' field
+        }
 
     def __init__(self, *args, **kwargs):
-        token = kwargs.pop('token', None)  # Get the token instance from kwargs
+        token = kwargs.pop('token', None)
         super(TokenForm, self).__init__(*args, **kwargs)
         
-        if token:  # If a token instance is provided, set the queryset for lemmas and senses
+        if token:
             self.fields['lemmas'].queryset = token.lemmas.all()
             self.fields['senses'].queryset = token.senses.all()
