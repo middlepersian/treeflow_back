ea40a25611397305d1b30d1205adb8b78a67f21f
Luke Günther <lguent12@uni-koeln.de>
2024-02-04 13:46:12 +0100

    Forcefully fix the uniqueness constraint

--- treeflow/search/migrations/0011_searchsession.py
+++ treeflow/search/migrations/0011_searchsession.py
@@ -21,7 +21,6 @@ class Migration(migrations.Migration):
                 ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False, unique=True)),
                 ('session_id', models.CharField(blank=True, max_length=255, null=True)),
                 ('results', django.contrib.postgres.fields.ArrayField(base_field=models.UUIDField(default=uuid.uuid4), blank=True, null=True, size=None)),
-                ('formset', models.ManyToManyField(blank=True, to='search.searchcriteria')),
                 ('user', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
