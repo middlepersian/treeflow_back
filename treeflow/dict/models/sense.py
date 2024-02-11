84bfb9d58b64d7f71eb66429ac749d045e33b586
Francisco Mondaca <f.mondaca@uni-koeln.de>
2024-02-11 15:28:15 +0100

    sense up

--- treeflow/dict/models/sense.py
+++ treeflow/dict/models/sense.py
@@ -9,7 +9,7 @@ class Sense(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid_lib.uuid4, editable=False)
     sense = models.TextField(null=True, blank=True, db_index=True)
     lemma_related = models.BooleanField(default=True)
-    language = models.CharField(max_length=15, blank=True, null=True, db_index=True)
+    language = models.CharField(max_length=3, blank=True, null=True, db_index=True)
     related_senses = models.ManyToManyField('self', blank=True, related_name='sense_related_senses')
     created_at = models.DateTimeField(auto_now_add=True)
     stage = models.CharField(max_length=10, blank=True)
@@ -26,7 +26,7 @@ class Sense(models.Model):
         ordering = ['sense']
 
     def __str__(self):
-        return '{} - {} - {}'.format(self.sense, self.language, self.lemma_related)
+        return '{} - {} - {}'.format(self.sense)
 
     def save(self, *args, **kwargs):
         # Normalize only the `normalized_field` before saving
