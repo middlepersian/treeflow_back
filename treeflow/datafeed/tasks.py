662d7f661fd31f081aa5f110f1881fa6ba804322
Francisco Mondaca <f.mondaca@uni-koeln.de>
2024-02-20 10:50:42 +0100

    improving caching

--- treeflow/datafeed/tasks.py
+++ treeflow/datafeed/tasks.py
@@ -1,10 +1,10 @@
-from huey.contrib.djhuey import task, periodic_task, db_task, db_periodic_task
+from huey.contrib.djhuey import task, periodic_task, db_task, db_periodic_task, enqueue
 from huey import crontab
 from huey import RedisHuey
 from django.core.cache import cache
 from treeflow.corpus.models import Text
 from treeflow.corpus.utils.zotero import request_zotero_api_for_collection
-from treeflow.datafeed.cache import update_zotero_data_in_cache, cache_all_texts, cache_all_zotero_sources
+from treeflow.datafeed.cache import update_zotero_data_in_cache, cache_all_texts, cache_all_zotero_sources, cache_sections_for_texts
 import logging
 
 logger = logging.getLogger(__name__)
@@ -20,7 +20,11 @@ def periodic_cache_all_zotero_sources():
     cache_all_zotero_sources()
 
 @db_periodic_task(crontab(hour='*', minute=0))
-def periodic_cache_all_texts():
-    logger.info("Running periodic_cache_all_texts task")
-    cache_all_texts()
-        
+def periodic_cache_all_texts_and_sections():
+    logger.info("Running combined periodic_cache tasks")
+    cache_texts_task = cache_all_texts.s()
+    cache_sections_task = cache_sections_for_texts.s()
+
+    # Chain the tasks
+    pipeline = cache_texts_task.then(cache_sections_task)
+    enqueue(pipeline)
