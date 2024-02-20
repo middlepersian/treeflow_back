9966b27757086751d8a74a1492976c0c06184b3c
Francisco Mondaca <f.mondaca@uni-koeln.de>
2024-02-20 14:15:45 +0100

    cache tasks up

--- treeflow/datafeed/tasks.py
+++ treeflow/datafeed/tasks.py
@@ -28,23 +28,17 @@ def cache_sections_for_texts_task():
     cache_sections_for_texts()
 
 @task()
-def clear_cache_task():
-    logger.info("Clearing cache")
-    cache.clear()
-
-@db_periodic_task(crontab(hour='*', minute=0))
-def periodic_cache_all_texts_and_sections_task():
+def periodic_texts_and_sections_task():
     logger.info("Running combined periodic_cache tasks")
-    clear_cache = clear_cache_task.s()
     cache_texts = cache_all_texts_task.s()
     cache_sections = cache_sections_for_texts_task.s()
 
     # Chain the tasks: clear cache -> cache all texts -> cache sections
-    pipeline = clear_cache.then(cache_texts).then(cache_sections)
+    pipeline = cache_texts.then(cache_sections)
     enqueue(pipeline)
 
-@db_periodic_task(crontab(hour='*', minute=0))
-def periodic_zotero_tasks_task():
+@task()
+def periodic_zotero_tasks():
     logger.info("Running combined Zotero periodic tasks")
     update_zotero_data = update_zotero_data_in_cache_task.s()
     cache_zotero_sources = cache_all_zotero_sources_task.s()
@@ -52,3 +46,14 @@ def periodic_zotero_tasks_task():
     # Chain the Zotero tasks
     zotero_pipeline = update_zotero_data.then(cache_zotero_sources)
     enqueue(zotero_pipeline)
+
+@periodic_task(crontab(hour='*', minute=0))
+def run_periodic_tasks():
+    # run combined periodic tasks
+    logger.info("Running combined periodic_cache tasks")
+    # clear cache
+    logger.info("Clearing cache")
+    cache.clear()
+    logger.info("Cache cleared")
+    periodic_texts_and_sections_task()
+    periodic_zotero_tasks()
