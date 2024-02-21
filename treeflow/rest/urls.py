c4084c7936a008d47dcfb7d6b7a1f24fef140c15
Francisco Mondaca <f.mondaca@uni-koeln.de>
2024-02-21 16:08:27 +0100

    rest app init

---
+++ treeflow/rest/urls.py
@@ -0,0 +1,10 @@
+# rest/urls.py
+from django.conf import settings
+from rest_framework.routers import DefaultRouter, SimpleRouter
+from test_drf.users.api.views import UserViewSet
+
+router = DefaultRouter() if settings.DEBUG else SimpleRouter()
+router.register("users", UserViewSet)
+
+app_name = "api"
+urlpatterns = router.urls
