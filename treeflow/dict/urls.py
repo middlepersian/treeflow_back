38126c39581adf916a5c76e62048fb37f4d5f0c6
Me9Rs <malte.rudius@web.de>
2024-02-22 12:57:10 +0100

    Added first stylistic scaffolding

--- treeflow/dict/urls.py
+++ treeflow/dict/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include, path
 from treeflow.dict.views.save_sense import save_sense
 from treeflow.dict.views.save_lemma import save_lemma
+from django.views.generic import TemplateView
 
 app_name = "treeflow.dict"
 urlpatterns = [
         path("save_sense/", save_sense, name="save_sense"),
         path("save_lemma/", save_lemma, name="save_lemma"),
+        path("dictionary/", TemplateView.as_view(template_name="../templates/dictionary.html"), name="dictionary"),
 ]
