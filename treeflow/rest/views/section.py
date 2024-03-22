36653fcf746b24e8ea01a48148cd1522dcedf6f5
Francisco Mondaca <f.mondaca@uni-koeln.de>
2024-03-22 16:13:14 +0100

    logging

--- treeflow/rest/views/section.py
+++ treeflow/rest/views/section.py
@@ -9,6 +9,10 @@ from rest_framework import status
 from treeflow.corpus.models import Section, Token
 from treeflow.rest.serializers.section import SectionSerializer, CABSectionSerializer
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 class SectionPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
@@ -27,23 +31,33 @@ class SectionViewSet(viewsets.ReadOnlyModelViewSet):
     @action(detail=False, methods=['get'], url_path='identifier/(?P<identifier>.+)', url_name='retrieve_by_identifier')
     @extend_schema(operation_id='retrieve_sections_by_identifier')
     def retrieve_by_identifier(self, request, identifier, *args, **kwargs):
-        sections = self.get_queryset().filter(identifier=identifier)
-        page = self.paginate_queryset(sections)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
+        logger.info(f"Retrieving sections by identifier: {identifier}")
+        try:
+            sections = self.get_queryset().filter(identifier=identifier)
+            page = self.paginate_queryset(sections)
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
 
-        serializer = self.get_serializer(sections, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-        
+            serializer = self.get_serializer(sections, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            logger.error(f"Error in retrieving sections by identifier: {identifier}", exc_info=True)
+            return Response({"detail": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
     @action(detail=False, methods=['get'], url_path='cab/(?P<identifier>.+)', url_name='retrieve_cab_tokens_by_identifier')
     @extend_schema(operation_id='retrieve_cab_tokens_by_identifier')
     def retrieve_cab_tokens_by_identifier(self, request, identifier, *args, **kwargs):
-        sections = self.get_queryset().filter(identifier=identifier).prefetch_related('tokens')
-        page = self.paginate_queryset(sections)
-        if page is not None:
-            serializer = CABSectionSerializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
+        logger.info(f"Retrieving CAB tokens by identifier: {identifier}")
+        try:
+            sections = self.get_queryset().filter(identifier=identifier).prefetch_related('tokens')
+            page = self.paginate_queryset(sections)
+            if page is not None:
+                serializer = CABSectionSerializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
 
-        serializer = CABSectionSerializer(sections, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+            serializer = CABSectionSerializer(sections, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            logger.error(f"Error in retrieving CAB sections by identifier: {identifier}", exc_info=True)
+            return Response({"detail": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
