a9364fb4610a7ca7286224e18f70c0831364b15f
Moritz Sebastian Hubert Eßer <messer19@uni-koeln.de>
2024-03-22 14:48:46 +0000

    Export

--- treeflow/corpus/views/export_text.py
+++ treeflow/corpus/views/export_text.py
@@ -8,7 +8,8 @@ from datetime import datetime
 
 import csv
 
-from django.http import StreamingHttpResponse
+from django.http import StreamingHttpResponse, JsonResponse
+from treeflow.datafeed.management.commands.export_text import text_to_conll
 # Set up logging
 logger = logging.getLogger(__name__)
 
@@ -22,141 +23,73 @@ class Echo:
         return value
 
 
-def resolve_text(request, text_id):
-    # get Text 
-    logger.debug(f"resolve_text: {text_id}")
+def resolve_state(request, text_id):
+    # check if user is authenticated
+    if not request.user.is_authenticated:
+        return JsonResponse({"status":"error","error": "User is not authenticated"})
+    
+    logger.debug(f"resolve_state: {text_id}")
     text = get_object_or_404(Text, id=text_id)
-    # check if text_csv is in cache
-    text_csv = cache.get(f"text_csv_{text_id}")
-    if not text_csv:
-         
-      text = Text.objects.prefetch_related(
-        'section_text',
-        'section_text__senses',
-        'section_text__tokens',
-        'section_text__tokens__lemmas',
-        'section_text__tokens__dependency_token',
-        'section_text__tokens__pos_token',
-        'section_text__tokens__feature_token',
-        'section_text__tokens__comment_token'
-      ).get(id=text_id)
-      # and prefetch all sections of type sentence
-      logger.debug(f"prefetching done")
-      sections = text.section_text.filter(type="sentence")
-      rows = []
-      for section in sections:
-        rows += sentenceToConnl(section)
+    text_cache_key = text.identifier+"_conll"
+    cached_data = cache.get(text_cache_key)
+    if not cached_data:
+        cache.set(text.identifier, {"status":"started","data":[]})
+        cached_data = cache.get(text.identifier)
+        text_to_conll(text=text, cache_key=text_cache_key)
+        return JsonResponse(cached_data)
+    
+    if cached_data["status"] == "pending":
+        return JsonResponse(cached_data)
+    
+    return JsonResponse({"status":"success","data":[],})
 
-      
-      text_csv = rows
-      # cache text_csv
-      cache.set(f"text_csv_{text_id}", text_csv, 60*60*24)
+def download_text(request, text_id):
+    # check if user is authenticated
+    if not request.user.is_authenticated:
+        return JsonResponse({"status":"error","error": "User is not authenticated"})
+    
+    logger.debug(f"download_text: {text_id}")
+    text = get_object_or_404(Text, id=text_id)
 
+    text_cache_key = text.identifier+"_conll"
+    cached_data = cache.get(text_cache_key)
+
+    if not cached_data:
+        # return error
+        return JsonResponse({"status":"error","error": "Data not found"})
+    if cached_data["status"] == "pending":
+        # return error
+        return JsonResponse({"status":"error","error": "Data is still being processed"})
+    
     pseudo_buffer = Echo()
     writer = csv.writer(pseudo_buffer, delimiter='\t')
     return StreamingHttpResponse(
-        (writer.writerow(row) for row in text_csv),
+        (writer.writerow(row) for row in cached_data["data"]),
         content_type="text/csv",
         headers={'Content-Disposition': f'attachment; filename="{text.identifier}.csv"'}
     )
 
-
-
-def resolve_sentence(request, section_id=None):
-    sentence = get_object_or_404(Section, id=section_id)
-    rows = sentenceToConnl(sentence)
+def resolve_text(request, text_id):
+    # check if user is authenticated
+    if not request.user.is_authenticated:
+        return JsonResponse({"status":"error","error": "User is not authenticated"})
+    
+    logger.debug(f"resolve_text: {text_id}")
+    text = get_object_or_404(Text, id=text_id)
+    text_cache_key = text.identifier+"_conll"
+    cached_data = cache.get(text_cache_key)
+    if not cached_data:
+        cache.set(text.identifier, {"status":"pending","data":[]},60*5)
+        text_to_conll(text=text, cache_key=text_cache_key)
+        cached_data = cache.get(text.identifier)
+    
+    if cached_data["status"] == "pending":
+        return JsonResponse(cached_data)
+    
     pseudo_buffer = Echo()
     writer = csv.writer(pseudo_buffer, delimiter='\t')
     return StreamingHttpResponse(
-        (writer.writerow(row) for row in rows),
+        (writer.writerow(row) for row in cached_data["data"]),
         content_type="text/csv",
-        headers={'Content-Disposition': 'attachment; filename="sentence.csv"'}
+        headers={'Content-Disposition': f'attachment; filename="{text.identifier}.csv"'}
     )
-
-def sentenceToConnl(sentence):
-    tokens = sentence.tokens.all()
-    rows = []
-    if tokens:
-      rows = [tokenToConnl(token) for token in tokens]
-
-    max_length = 20
-
-    id_row = ['_']*max_length
-    id_row[0] = f'#SENTENCE_ID = {sentence.identifier}'
-
-    for translation in sentence.senses.all():
-        translation_row = ['_']*max_length
-        translation_row[0] = f'#TRANSLATION = {translation}'
-
-        rows.insert(0, translation_row)
-    rows.insert(0, id_row)
-    rows.append(['']*max_length)
-
-    return rows
-
-
-def tokenToConnl(token):
-    sections = token.section_tokens.all()
-    lemmas = token.lemmas.all()
-
-    number_in_sentence = str(token.number_in_sentence) if token.number_in_sentence else "_"
-    transcription = token.transcription if token.transcription else "_"
-    head = "|".join([str(dep.head_number) if dep.head_number else "_" for dep in token.dependency_token.all() if not dep.enhanced])
-    deprel = "|".join([str(dep.rel) if dep.rel else "_" for dep in token.dependency_token.all() if not dep.enhanced])
-    deps = "|".join([f"{str(dep.head_number)}:{str(dep.rel)}" if dep.rel and dep.head_number else "_" for dep in token.dependency_token.all() if dep.enhanced])
-    transliteration = str(token.transliteration) if token.transliteration else "_"
-    # folio
-    line = "|".join([str(line.number) if line.number else "_" for line in sections if line.type == "line"])
-    newParts = "|".join([str(section.identifier) for section in sections if section.type != "sentence" and section.type != "line"])
-    
-    # Lemmas
-    lemmaList =  [(str(lemma.word), str(lemma.language)) for lemma in lemmas]
-
-    lemmas = "|".join([lemma[0] for lemma in lemmaList])
-    lemmaLangs = "|".join([lemma[1] for lemma in lemmaList])
-
-    avestan = str(token.avestan) if token.avestan else "_"
-    senses = "|".join([str(sense.sense) if sense.sense else "_" for sense in token.senses.all()])
-    pos = "|".join([str(pos.pos) if pos.pos else "_" for pos in token.pos_token.all()])
-    features = "|".join([f"{str(feature.feature)}={str(feature.feature_value)}" for feature in token.feature_token.all()])
-    
-    commentFields = packComments(token)
-    # term_tech
-    uncertrain = commentFields[1]
-    comment = commentFields[0]
-    # literature ?
-    newSuggestions = commentFields[2]
-    discussion = commentFields[3]
-    # gloss
-    language = str(token.language) if token.language else "_"
-    senses_lang = "|".join([str(sense.language) if sense.language else "_" for sense in token.senses.all()])
-
-    return [number_in_sentence,
-                       transcription,
-                         head,
-                           deprel,
-                             deps,
-                               transliteration,
-                                 line,
-                                   newParts, 
-                                   lemmas,
-                                     lemmaLangs,
-                                       avestan,
-                                         senses,
-                                           pos,
-                                             features,
-                                               uncertrain,
-                                                 comment,
-                                                   newSuggestions,
-                                                     discussion,
-                                                       language,
-                                                         senses_lang]
-
-def packComments(token):
-    comments = token.comment_token.all()
-    comment_col = "|".join([f"{str(comment.comment)}" if comment.comment else "_" for comment in comments])
-    uncertain_col = "|".join([f"{str(comment.uncertain)}" if comment.uncertain else "_" for comment in comments])
-    new_suggestion_col = "|".join([f"{str(comment.new_suggestion)}" if comment.new_suggestion else "_" for comment in comments])
-    discussion_col = "|".join([f"{str(comment.to_discuss)}" if comment.to_discuss else "_" for comment in comments])
-
-    return comment_col, uncertain_col, new_suggestion_col, discussion_col
