fb0b04fd7d1b0fd1efa7c28c4c2bec479ddabc8c
Me9Rs <malte.rudius@web.de>
2024-04-08 11:43:11 +0200

    Added related sense/lemma functionality

--- treeflow/dict/views/update_lemma.py
+++ treeflow/dict/views/update_lemma.py
@@ -1,15 +1,18 @@
 import logging
-from django.http import JsonResponse, HttpResponse
-from django.shortcuts import get_object_or_404
-from treeflow.dict.models import Lemma
+
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404, redirect
 from django.template.loader import render_to_string
-from django.shortcuts import redirect
+
+from treeflow.dict.models import Lemma
+from treeflow.dict.models.sense import Sense
 
 # Set up logging
 logger = logging.getLogger(__name__)
 
 def update_lemma(request, lemma_id):
     logger.debug(f"Request method: {request.method}")
+    logger.debug(f"Request method: {request.POST}")
     logger.debug(f"Lemma ID: {lemma_id}")
 
     try:
@@ -39,7 +42,28 @@ def update_lemma(request, lemma_id):
                 new_categories = request.POST.getlist('categories')
                 logger.info(f"Updating categories for lemma with ID {lemma_id} to {new_categories}")
                 lemma.categories = new_categories
-            
+
+            # Handling related senses
+            related_sense_ids = request.POST.getlist('related_senses')
+            if related_sense_ids.__len__() > 0:
+                related_senses = Sense.objects.filter(pk__in=related_sense_ids)
+                lemma.related_senses.set(related_senses)
+                logger.info(f"Updated related senses for lemma with ID {lemma_id} to {related_sense_ids}")
+            else:
+                lemma.related_senses.clear()
+                logger.info(f"Removed all related senses for lemma with ID {lemma_id}")
+
+            # Handling adding related lemmas
+            related_lemma_ids = request.POST.getlist('related_lemmas')
+            logger.debug(f"related_lemma_ids: {related_lemma_ids}")
+            if related_lemma_ids.__len__() > 0:
+                related_lemmas = Lemma.objects.filter(pk__in=related_lemma_ids)
+                lemma.related_lemmas.set(related_lemmas)
+                logger.info(f"Updated related lemmas for lemma with ID {lemma_id} to {related_lemma_ids}")
+            else:
+                lemma.related_lemmas.clear()
+                logger.info(f"Removed all related lemmas for lemma with ID {lemma_id}")
+
             lemma.save()
 
             updated_lemma_html = render_to_string('lemma_details.html', {'lemma': lemma})
