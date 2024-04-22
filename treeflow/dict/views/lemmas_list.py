5c70abff2aafc3300be497bbfdc4f9e5dcbc7294
Me9Rs <malte.rudius@web.de>
2024-04-22 13:49:00 +0200

    Added usage of new cahche system

--- treeflow/dict/views/lemmas_list.py
+++ treeflow/dict/views/lemmas_list.py
@@ -1,11 +1,14 @@
+from django.core.cache import cache
 from django.shortcuts import render
 
 from treeflow.dict.models.lemma import Lemma
 
-
 def lemmas_list(request, lemma_id = None):
-    lemmas = Lemma.objects.values('id', 'word')
-    
+    lemmas = cache.get('lemmas')
+
+    if lemmas is None:
+        lemmas = Lemma.objects.values('id', 'word')
+        
     context = {
         'lemmas': lemmas,
     }
