285101475054592c98b0b4c6804fa2785480e720
Francisco Mondaca <f.mondaca@uni-koeln.de>
2024-04-30 13:14:16 +0200

    unused post form

--- treeflow/corpus/views/texts.py
+++ treeflow/corpus/views/texts.py
@@ -1,7 +1,6 @@
 from django.core.cache import cache
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from treeflow.corpus.forms.text_form import TextForm
-from treeflow.corpus.models import Text
 from treeflow.datafeed.cache import cache_all_texts 
 import logging
 
@@ -20,26 +19,7 @@ def texts_view(request):
 
     logger.debug("request method: %s", request.method)
 
-    if request.method == 'POST':
-        text_id = request.POST.get('text_id')
-        logger.debug("text_id: %s", text_id)
-        if text_id:
-            text = get_object_or_404(Text, pk=text_id)
-            form = TextForm(request.POST, instance=text)
-        else:
-            form = TextForm(request.POST)
-
-        if form.is_valid():
-            saved_text = form.save()
-            cache.delete(cache_key_texts)  # Invalidate cache
-            if request.headers.get('HX-Request'):
-                html = render_to_string('text_row.html', {'text': saved_text})
-                return JsonResponse({'html': html})
-        else:
-            pass
-
-    else:
-        form = TextForm()
+    form = TextForm()
 
     context = {'texts': texts, 'form': form}
     return render(request, 'texts.html', context)
