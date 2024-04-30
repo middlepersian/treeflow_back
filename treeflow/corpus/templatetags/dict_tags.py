4c1194d5b622cba4d7c872b84ea91f16056202ea
Moritz Sebastian Hubert Eßer <messer19@uni-koeln.de>
2024-04-30 10:37:59 +0000

    Update dict_tags.py

--- treeflow/corpus/templatetags/dict_tags.py
+++ treeflow/corpus/templatetags/dict_tags.py
@@ -34,15 +34,15 @@ def customABCSort(data):
     'z', 'ź', 'ż', 'ž'
 ]
     
-    def check_for_index(word:str) -> int:
-        if not word:
+    def check_for_index(x) -> int:
+        if not x['word']:
             return 99999999999
-        if word['0'] not in custom_alphabetical_order:
+        if x['word'][0] not in custom_alphabetical_order:
             return 99999999999
         return custom_alphabetical_order.index(x['word'][0])
 
     try:
-        sorted_data = sorted(data.values(), key=lambda x: check_for_index)
+        sorted_data = sorted(data.values(), key=lambda x: check_for_index(x))
     except Exception as e:
         print(f"dict_tags error occured: {e}")
         sorted_data = []
