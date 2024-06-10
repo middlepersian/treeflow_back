de01db401d4a0b1bf3cb344290c02f462d12866d
Moritz Esser <moritz.esser@uni-koeln.de>
2024-06-10 15:47:34 +0200

    feat: Add section merge functionality
    This commit adds a new function `section_merge` to the `section_editor.py` file in the `corpus/views` directory. The `section_merge` function allows merging two sections of type "sentence" by updating the section's next pointer and renumbering the tokens. It also includes error handling for invalid section types and missing next sections.

--- treeflow/corpus/views/section_editor.py
+++ treeflow/corpus/views/section_editor.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
 from treeflow.corpus.models import Section
 from treeflow.corpus.forms.section_editor_form import SectionEditorForm
 from treeflow.dict.forms.sense_form import SenseForm  # Import the SenseForm
@@ -19,3 +20,49 @@ def section_editor_form_view(request, section_id=None):
     sense_form = SenseForm()  # Create an instance of SenseForm
 
     return render(request, 'section_editor_form.html', {'form': form, 'sense_form': sense_form, 'section_id': section_id})
+
+
+def section_merge(request):
+    """ 
+        Merge two sections of type "sentence"
+        Post Request, requires section_id
+        
+    """
+    logger.debug("section_merge")
+    section_id = request.POST.get('section_id')
+    section_source = get_object_or_404(Section,id=section_id)
+    if section_source.type != "sentence":
+        return JsonResponse({'status': 'error', 'message': 'Section is not of type sentence'})
+    if not section_source.next:
+        return JsonResponse({'status': 'error', 'message': 'Section has no next section'})
+
+    section_target = Section.objects.get(id=section_source.next)
+    if section_target.type != "sentence":
+        return JsonResponse({'status': 'error', 'message': 'Next section is not of type sentence'})
+    
+    logger.debug("source: %s" % section_source, "target: %s" % section_target)
+
+    # targets next is now sources next
+    new_next = section_target.next
+    section_source.next = new_next
+
+    # get highest token number in source        
+    last_token_source_number = 0.0
+    last_token_source = section_source.tokens.last()
+    if last_token_source:
+        last_token_source_number = last_token_source.number_in_sentence
+    
+    # add all tokens from target to source and renumber them
+    for token in section_target.tokens.all():
+        logger.debug("token: %s" % token)
+        section_source.tokens.add(token)
+        section_target.tokens.remove(token)
+        last_token_source_number += 1.0
+        token.number_in_sentence = last_token_source_number
+        token.save()
+    
+    # save changes
+    section_source.save()
+    section_target.save()
+
+    return JsonResponse({'status': 'success', 'message': 'Sections merged successfully'})
