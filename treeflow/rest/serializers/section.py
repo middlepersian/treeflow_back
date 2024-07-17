87706f8c46b7da00ce11a2d88522c7f2c3917b3f
Moritz Esser <moritz.esser@uni-koeln.de>
2024-07-17 12:20:53 +0200

    Update API behaviour for type==subsubsection

--- treeflow/rest/serializers/section.py
+++ treeflow/rest/serializers/section.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from treeflow.corpus.models import Section, Token
+from treeflow.corpus.management.commands.newparts import get_tokens_between_subsections
 
 
 class SectionTokenSerializer(serializers.ModelSerializer):
@@ -11,7 +12,17 @@ class SectionTokenSerializer(serializers.ModelSerializer):
 class SectionSerializer(serializers.ModelSerializer):
 
     text_identifier = serializers.CharField(source='text.identifier', read_only=True)
-    tokens = SectionTokenSerializer(many=True, read_only=True)
+
+    def get_tokens(self, obj):
+        if obj.type == "subsubsection":
+            # Call your method to get the tokens for "subsubsection"
+            tokens = get_tokens_between_subsections(obj.identifier)
+        else:
+            # Return the regular tokens for other types
+            tokens = obj.tokens.all()
+        return SectionTokenSerializer(tokens, many=True).data
+    
+    tokens = serializers.SerializerMethodField()
     class Meta:
         model = Section
         fields = '__all__'
